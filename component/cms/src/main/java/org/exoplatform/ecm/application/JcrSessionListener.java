d91d5dbafe3dd87066c07245428fc4b72ff2712b
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2007-07-25 06:37:07 +0000

    add JCRSessionListner to logout all jcr session

---
+++ component/cms/src/main/java/org/exoplatform/ecm/application/JcrSessionListener.java
@@ -0,0 +1,38 @@
+/***************************************************************************
+ * Copyright 2001-2007 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.ecm.application;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.RootContainer;
+import org.exoplatform.services.jcr.ext.app.SessionProviderService;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : Hoa Pham	
+ *          hoa.pham@exoplatform.com
+ * Jul 24, 2007  
+ */
+public class JcrSessionListener implements HttpSessionListener {
+
+  public void sessionCreated(HttpSessionEvent event) {    
+  }
+
+  public void sessionDestroyed(HttpSessionEvent event) {
+    HttpSession session = event.getSession() ;    
+    ServletContext context = session.getServletContext() ;    
+    PortalContainer pcontainer = 
+      RootContainer.getInstance().getPortalContainer(context.getServletContextName()) ;
+    PortalContainer.setInstance(pcontainer) ;       
+    SessionProviderService providerService = (SessionProviderService)pcontainer.getComponentAdapterOfType(SessionProviderService.class) ;    
+    providerService.removeSessionProvider(session.getId()) ;
+    PortalContainer.setInstance(null) ;           
+  }
+
+}
