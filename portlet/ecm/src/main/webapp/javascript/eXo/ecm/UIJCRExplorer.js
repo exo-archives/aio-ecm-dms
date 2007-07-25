31eac5fecffd20b0802518bd2c6145750758e2a4
Philippe Aristote <philippe.aristote@exoplatform.com>
2007-07-25 09:06:37 +0000

    added scroll tabs to ViewNodeType and ViewFilePlan

--- portlet/ecm/src/main/webapp/javascript/eXo/ecm/UIJCRExplorer.js
+++ portlet/ecm/src/main/webapp/javascript/eXo/ecm/UIJCRExplorer.js
@@ -1,74 +1,73 @@
 
 function UIJCRExplorer() {
-	this.scrollMgr = null;
+	this.vnScrollMgr = null;
 	this.ntScrollMgr = null;
 };
 
-UIJCRExplorer.prototype.addCallBackToLink = function(elementId) {
-	if (elementId) {
-		var rootElement = document.getElementById(elementId);
-		var links = rootElement.getElementsByTagName("a");
-		for (var i=0; i<links.length; i++) {
-			var link = links[i];
-			var url = link.href;
-			if (url && url.indexOf('op=View') != -1 && url.indexOf('loadScroll') == -1) {
-				url = url.substr(0, url.length-1).concat(", eXo.ecm.UIJCRExplorer.loadScroll)");
-	  		link.href = url;
-			}
-		}
-	}
-};
+//UIJCRExplorer.prototype.addCallBackToViewNodeLink = function() {
+//	var rootElement = document.getElementById("UIJCRExplorer");
+//	var searchResult = eXo.core.DOMUtil.findFirstDescendantByClass(rootElement, "div", "UISearchResult");
+//	if (searchResult) {
+//		var links = searchResult.getElementsByTagName("a");
+//		for (var i=0; i<links.length; i++) {
+//			var link = links[i];
+//			var url = link.href;
+//			if (url && url.indexOf('op=View') != -1 && url.indexOf('loadViewNodeScroll') == -1) {
+//				url = url.substr(0, url.length-1).concat(", eXo.ecm.UIJCRExplorer.loadViewNodeScroll)");
+//	  		link.href = url;
+//			}
+//		}
+//	}
+//};
 
-UIJCRExplorer.prototype.addCallBackToNodeTypeLink = function() {
-	var rootElement = document.getElementById("UIActionBar");
-	if (rootElement) {
-		var links = rootElement.getElementsByTagName("a");
-		for (var i=0; i<links.length; i++) {
-			var link = links[i];
-			var url = link.href;
-			if (url && url.indexOf('ViewNodeType') != -1 && url.indexOf('loadScroll') == -1) {
-				url = url.substr(0, url.length-1).concat(", eXo.ecm.UIJCRExplorer.loadScroll)");
-	  		link.href = url;
-			}
-		}
-	}
-};
+//UIJCRExplorer.prototype.addCallBackToNodeTypeLink = function() {
+//	var rootElement = document.getElementById("UIJCRExplorer");
+//	var actionBar = eXo.core.DOMUtil.findFirstDescendantByClass(rootElement, "div", "UIActionBar");
+//	if (actionBar) {
+//		var links = actionBar.getElementsByTagName("a");
+//		for (var i=0; i<links.length; i++) {
+//			var link = links[i];
+//			var url = link.href;
+//			if (url && url.indexOf('ViewNodeType') != -1 && url.indexOf('loadNodeTypeScroll') == -1) {
+//				url = url.substr(0, url.length-1).concat(", eXo.ecm.UIJCRExplorer.loadNodeTypeScroll)");
+//	  		link.href = url;
+//			}
+//		}
+//	}
+//};
 
-UIJCRExplorer.prototype.loadScroll = function(e) {
-//	console.log("load scroll jcr");
+UIJCRExplorer.prototype.loadViewNodeScroll = function(e) {
 	var jcr = eXo.ecm.UIJCRExplorer;
-	//var uiDocumentWorkspace = document.getElementById("UIDocumentWorkspace");
 	var uiFilePlanView = document.getElementById("UIFilePlanView");
-	//var rootNode = (uiDocumentWorkspace != null) ? uiDocumentWorkspace : ((uiFilePlanView != null) ? uiFilePlanView : null);
 	if (uiFilePlanView) {
-		jcr.scrollMgr = eXo.portal.UIPortalControl.newScrollManager();
-		jcr.scrollMgr.initFunction = jcr.initScroll;
+		jcr.vnScrollMgr = eXo.portal.UIPortalControl.newScrollManager();
+		jcr.vnScrollMgr.initFunction = jcr.initViewNodeScroll;
 		var mainCont = eXo.core.DOMUtil.findFirstDescendantByClass(uiFilePlanView, "div", "UIHorizontalTabs");
 		var tabs = eXo.core.DOMUtil.findFirstDescendantByClass(mainCont, "div", "TabsContainer");
 		var arrows = eXo.core.DOMUtil.findFirstDescendantByClass(mainCont, "div", "NavigationButtonContainer");
-		jcr.scrollMgr.mainContainer = mainCont;
-		jcr.scrollMgr.arrowsContainer = arrows;
-		jcr.scrollMgr.loadElements("UITab");
+		jcr.vnScrollMgr.mainContainer = mainCont;
+		jcr.vnScrollMgr.arrowsContainer = arrows;
+		jcr.vnScrollMgr.loadElements("UITab");
 		var arrowButtons = eXo.core.DOMUtil.findDescendantsByTagName(arrows, "div");
 		if (arrowButtons.length == 2) {
-			jcr.scrollMgr.initArrowButton(arrowButtons[0], "left", "NavigationIcon ScrollBackArrow16x16Icon", "NavigationIcon DisableBackArrow16x16Icon", "NavigationIcon DisableBackArrow16x16Icon");
-			jcr.scrollMgr.initArrowButton(arrowButtons[1], "right", "NavigationIcon ScrollNextArrow16x16Icon", "NavigationIcon DisableNextArrow16x16Icon", "NavigationIcon DisableNextArrow16x16Icon");
+			jcr.vnScrollMgr.initArrowButton(arrowButtons[0], "left", "NavigationIcon ScrollBackArrow16x16Icon", "NavigationIcon DisableBackArrow16x16Icon", "NavigationIcon DisableBackArrow16x16Icon");
+			jcr.vnScrollMgr.initArrowButton(arrowButtons[1], "right", "NavigationIcon ScrollNextArrow16x16Icon", "NavigationIcon DisableNextArrow16x16Icon", "NavigationIcon DisableNextArrow16x16Icon");
 		}
-		jcr.scrollMgr.callback = jcr.scrollCallback;
-		jcr.initScroll();
+		jcr.vnScrollMgr.callback = jcr.viewNodeScrollCallback;
+		jcr.initViewNodeScroll();
 	}
 };
 
-UIJCRExplorer.prototype.initScroll = function(e) {
-	var scrollMgr = eXo.ecm.UIJCRExplorer.scrollMgr;
+UIJCRExplorer.prototype.initViewNodeScroll = function(e) {
+	var scrollMgr = eXo.ecm.UIJCRExplorer.vnScrollMgr;
 	scrollMgr.init();
 	// Gets the maximum width available for the tabs
 	scrollMgr.checkAvailableSpace();
 	scrollMgr.renderElements();
 };
 
-UIJCRExplorer.prototype.scrollCallback = function() {
-	var scrollMgr = eXo.ecm.UIJCRExplorer.scrollMgr;
+UIJCRExplorer.prototype.viewNodeScrollCallback = function() {
+	var scrollMgr = eXo.ecm.UIJCRExplorer.vnScrollMgr;
 	var selTab = eXo.core.DOMUtil.findFirstDescendantByClass(scrollMgr.mainContainer, "div", "SelectedTab");
 	if (selTab) {
 		scrollMgr.cleanElements();
@@ -77,10 +76,11 @@ UIJCRExplorer.prototype.scrollCallback = function() {
 };
 
 UIJCRExplorer.prototype.loadNodeTypeScroll = function() {
+	console.log("load nt scroll");
 	var jcr = eXo.ecm.UIJCRExplorer;
 	var uiPopup = document.getElementById("UINodeTypeInfoPopup");
 	if (uiPopup) {
-		console.log("load nt scroll");
+		console.log("found UINodeTypeInfoPopup");
 		jcr.ntScrollMgr = eXo.portal.UIPortalControl.newScrollManager();
 		jcr.ntScrollMgr.initFunction = jcr.initNodeTypeScroll;
 		var mainCont = eXo.core.DOMUtil.findFirstDescendantByClass(uiPopup, "div", "UIHorizontalTabs");
@@ -100,11 +100,11 @@ UIJCRExplorer.prototype.loadNodeTypeScroll = function() {
 };
 
 UIJCRExplorer.prototype.initNodeTypeScroll = function() {
-	var ntScrollMgr = eXo.ecm.UIJCRExplorer.ntScrollMgr;
-	ntScrollMgr.init();
+	var scrollMgr = eXo.ecm.UIJCRExplorer.ntScrollMgr;
+	scrollMgr.init();
 	// Gets the maximum width available for the tabs
-	ntScrollMgr.checkAvailableSpace();
-	ntScrollMgr.renderElements();
+	scrollMgr.checkAvailableSpace();
+	scrollMgr.renderElements();
 };
 
 eXo.ecm.UIJCRExplorer = new UIJCRExplorer();
