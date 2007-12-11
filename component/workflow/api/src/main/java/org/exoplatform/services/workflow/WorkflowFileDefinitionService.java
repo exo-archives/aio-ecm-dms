02a06a6987d845a678db1374af165a1bb16d11df
John Doe <noreply@exoplatform.com>
2007-12-11 08:54:06 +0000

    change copyright

--- component/workflow/api/src/main/java/org/exoplatform/services/workflow/WorkflowFileDefinitionService.java
+++ component/workflow/api/src/main/java/org/exoplatform/services/workflow/WorkflowFileDefinitionService.java
@@ -24,41 +24,11 @@ package org.exoplatform.services.workflow;
  * which makes it possible to allow various types of storage (eg: File System,
  * ECM).
  * 
- * <i>This interface is currently part of the Bonita /*
- * Copyright (C) 2003-2007 eXo Platform SAS.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see<http://www.gnu.org/licenses/>.
- */
-package as jBPM has a 
+ * <i>This interface is currently part of the Bonita 
+ * package as jBPM has a 
  * built-in facility to manage File Definition. It may however be a good idea to
- * move it to the api /*
- * Copyright (C) 2003-2007 eXo Platform SAS.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see<http://www.gnu.org/licenses/>.
- */
-package to make things common.</i>
+ * move it to the api 
+ * package to make things common.</i>
  * 
  * Created by Bull R&D
  * @author Brice Revenant
