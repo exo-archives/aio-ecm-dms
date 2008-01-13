49ea964a75c541af2790ff6d1112b07dadcde491
benjamin_mestrallet <benjamin_mestrallet>
2008-01-13 21:50:25 +0000

    merge CSS in production environment to improve performance

--- portlet/workflow/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ portlet/workflow/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -2,35 +2,41 @@
 SkinService.addSkin(
     "workflow/WorkflowControllerPortlet",
     "Default",
-    "/workflow/skin/webui/component/DefaultStylesheet.css"
+    "/workflow/skin/webui/component/DefaultStylesheet.css",
+    ServletContext
 ) ;
 
 SkinService.addSkin(
     "workflow/WorkflowControllerPortlet",
     "Vista",
-    "/workflow/skin/webui/component/VistaStylesheet.css"
+    "/workflow/skin/webui/component/VistaStylesheet.css",
+    ServletContext
 ) ;
  
 SkinService.addSkin(
     "workflow/WorkflowControllerPortlet",
     "Mac",
-    "/workflow/skin/webui/component/MacStylesheet.css"
+    "/workflow/skin/webui/component/MacStylesheet.css",
+    ServletContext
 ) ;
 
 SkinService.addSkin(
     "workflow/WorkflowAdministrationPortlet",
     "Default",
-    "/workflow/skin/webui/component/DefaultStylesheet.css"
+    "/workflow/skin/webui/component/DefaultStylesheet.css",
+    ServletContext
 ) ;
 
 SkinService.addSkin(
     "workflow/WorkflowAdministrationPortlet",
     "Vista",
-    "/workflow/skin/webui/component/VistaStylesheet.css"
+    "/workflow/skin/webui/component/VistaStylesheet.css",
+    ServletContext
 ) ;
  
 SkinService.addSkin(
     "workflow/WorkflowAdministrationPortlet",
     "Mac",
-    "/workflow/skin/webui/component/MacStylesheet.css"
+    "/workflow/skin/webui/component/MacStylesheet.css",
+    ServletContext
 ) ;
