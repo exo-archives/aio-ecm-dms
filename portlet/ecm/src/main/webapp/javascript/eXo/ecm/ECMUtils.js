d7f1145194b3cabeb6466cb0d332ddc30b18fff3
Minh Van Dang <minh.dang@exoplatform.com>
2008-04-02 11:23:24 +0000

    Fix for filter in search form

--- portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
+++ portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
@@ -133,10 +133,67 @@ ECMUtils.prototype.collapseExpand = function(elemt) {
 	}
 	return true;
 };
+	var discoverEXO = function(object) {
+		removeInfoDiv();
+		if (!object) return;
+		var infoDiv = document.createElement("div") ;
+			infoDiv.setAttribute("id" , "show.EX0.0bject") ;
+			infoDiv.style.background = "black" ;
+			infoDiv.style.bottom = "0px" ;
+			infoDiv.style.color = "white" ;
+			infoDiv.style.padding = "3px" ;
+			infoDiv.style.position = "absolute" ;
+			infoDiv.style.zIndex = "9999" ;
+		var closeButton = document.createElement("div") ;
+			closeButton.setAttribute("id" , "Remove.Information.0bject") ;
+			closeButton.style.paddingBottom = "10px" ;
+			closeButton.style.textAlign = "right" ;
+			closeButton.innerHTML = "<span style='color: red; font-weight: bold; cursor: pointer'>[ X ]</span>" ;
+		var blockContent = 	document.createElement("div") ;
+			blockContent.style.background = "#848484" ;
+			blockContent.style.border = "1px solid green" ;
+			blockContent.style.height = "300px" ;
+			blockContent.style.overflow = "auto" ;
+			blockContent.style.padding = "10px" ;
+			blockContent.style.width = "400px" ;
+		var csHTML = new String() ;
+		var csObject = new String() ;
+		if (typeof object == "object") {
+			for (var exo in object) {
+				if (typeof object[exo] == "string") csObject = object[exo].replace(/</g, "&lt;") ;
+				else csObject = object[exo];
+				csHTML +=  "<span style='color: #9b1a00'>" + exo + "</span> : " + csObject + "<br />" ;
+			}
+		} else if (typeof object == "string") {
+			csHTML = object.replace(/</g, "&lt;") ;
+		} else {
+			csHTML = object.toString();
+		}
+		
+		document.body.appendChild(infoDiv) ;
+		infoDiv.appendChild(closeButton) ;
+		infoDiv.appendChild(blockContent) ;
+		blockContent.innerHTML = csHTML ;
+		
+		closeButton = document.getElementById("Remove.Information.0bject") ;
+		closeButton.childNodes[0].onclick = removeInfoDiv ;
+		
+		function removeInfoDiv() {
+		 if (document.getElementById("show.EX0.0bject")) {
+			var infoDiv = document.getElementById("show.EX0.0bject") ;
+			infoDiv.parentNode.removeChild(infoDiv) ;
+			}
+		}
+	} ;
 
 ECMUtils.prototype.filterValue = function(frmId) {
 	var form = document.getElementById(frmId) ;
-	form['result'].innerHTML = form['tempSel'].innerHTML ;
+	if (eXo.core.Browser.browserType == "ie") {
+		var text = document.createTextNode(form['tempSel'].innerHTML);
+		form['result'].appendChild(text);
+	}else {
+	  form['result'].innerHTML = form['tempSel'].innerHTML ;
+	}
 	var	filterValue = form['filter'].value ;
 	filterValue = filterValue.replace("*", ".*") ;		
 	var re = new RegExp(filterValue, "i") ;	
