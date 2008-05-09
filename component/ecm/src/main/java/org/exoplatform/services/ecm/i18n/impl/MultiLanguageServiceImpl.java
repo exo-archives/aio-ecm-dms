2e10c844fe0974570b77cdd1a922d865aaa8a71a
Ly Dinh Quang <quang.ly@exoplatform.com>
2008-05-09 03:13:49 +0000

    Create MultiLanguageServiceImpl

---
+++ component/ecm/src/main/java/org/exoplatform/services/ecm/i18n/impl/MultiLanguageServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2003-2008 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.services.ecm.i18n.impl;
+
+import javax.jcr.Node;
+
+import org.exoplatform.services.ecm.i18n.MultiLanguageService;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : Ly Dinh Quang
+ *          quang.ly@exoplatform.com
+ *			    xxx5669@yahoo.com
+ * May 7, 2008  
+ */
+public class MultiLanguageServiceImpl implements MultiLanguageService {
+
+  public String getDefaultLanguage(Node document) throws Exception {
+    return null;
+  }  
+}
