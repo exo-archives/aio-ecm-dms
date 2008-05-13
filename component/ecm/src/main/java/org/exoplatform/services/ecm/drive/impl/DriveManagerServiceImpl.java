9324e7bacf500522da69bac9b7f726f3903a490e
Dang Tan Dung <dung.dang@exoplatform.com>
2008-05-13 08:26:40 +0000

    Dang Tan Dung commit DriveManagerServiceImpl

--- component/ecm/src/main/java/org/exoplatform/services/ecm/drive/impl/DriveManagerServiceImpl.java
+++ component/ecm/src/main/java/org/exoplatform/services/ecm/drive/impl/DriveManagerServiceImpl.java
@@ -17,20 +17,28 @@
 package org.exoplatform.services.ecm.drive.impl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Session;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
 
+import org.apache.commons.logging.Log;
+import org.exoplatform.container.component.ComponentPlugin;
+import org.exoplatform.container.xml.ObjectParameter;
+import org.exoplatform.services.ecm.access.PermissionManagerService;
 import org.exoplatform.services.ecm.drive.DriveEntry;
 import org.exoplatform.services.ecm.drive.DriveManagerService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.jcr.ext.registry.RegistryEntry;
 import org.exoplatform.services.jcr.ext.registry.RegistryService;
+import org.exoplatform.services.log.ExoLogger;
+import org.picocontainer.Startable;
 import org.w3c.dom.Element;
 
 /**
@@ -39,23 +47,37 @@ import org.w3c.dom.Element;
  *          dzungdev@gmail.com
  * May 8, 2008  
  */
-public class DriveManagerServiceImpl implements DriveManagerService{
+public class DriveManagerServiceImpl implements DriveManagerService, Startable {
 
   private static String WORKSPACE = "exo:workspace".intern() ;
   private static String PERMISSIONS = "exo:accessPermissions".intern() ;
   private static String VIEWS = "exo:views".intern() ;
-  private static String ICON = "exo:icon".intern() ;
-  private static String PATH = "exo:path".intern() ;
-  private static String SHOW_REFERENCES = "exo:viewPreferences".intern() ;
-  private static String SHOW_NON_DOCUMENT = "exo:viewNonDocument".intern() ;
-  private static String SHOW_SIDEBAR = "exo:viewSideBar".intern() ;
+  private static String ICON = "exo:iconPath".intern() ;
+  private static String PATH = "exo:homePath".intern() ;
+  private static String SHOW_REFERENCES = "exo:showReference".intern() ;
+  private static String SHOW_NON_DOCUMENT = "exo:showNonDocument".intern() ;
+  private static String SHOW_SIDEBAR = "exo:showSideBar".intern() ;
   private static String SHOW_HIDDEN_NODE = "exo:showHiddenNode".intern() ;
   private static String ALLOW_CREATE_FOLDER = "exo:allowCreateFolder".intern() ;
+  private static String DRIVE_NAME = "exo:name".intern() ;
+  private static String GROUP = "exo:group".intern() ;
+  private static String REPOSITORY = "exo:repository".intern() ;
 
   private RegistryService registryService_ ;
+  private PermissionManagerService permissionMgtService_ ;
+  private List<DriveManagerPlugin> drivePlugins_ = new ArrayList<DriveManagerPlugin>() ; 
 
-  public DriveManagerServiceImpl(RegistryService registryService) {
+  private Log log_ = ExoLogger.getLogger("ecm:driveManagerService") ;
+
+  public DriveManagerServiceImpl(RegistryService registryService, PermissionManagerService permissionMgtService) {
     this.registryService_ = registryService ;
+    this.permissionMgtService_ = permissionMgtService ;
+  }
+
+  public void addPlugin(ComponentPlugin plugin) {
+    if(plugin instanceof DriveManagerPlugin) {      
+      drivePlugins_.add((DriveManagerPlugin)plugin) ;
+    }
   }
 
   public void addDrive(DriveEntry drive, SessionProvider sessionProvider) throws Exception {
@@ -63,36 +85,12 @@ public class DriveManagerServiceImpl implements DriveManagerService{
     String entryPath = DRIVE_REGISTRY_PATH + "/" + drive.getGroup() + "/" +drive.getName();
     RegistryEntry registryEntry ;
     try {
-      registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;
-      Element element = registryEntry.getDocument().getDocumentElement() ;
-      setXmlNameSpace(element, "xmlns:exo", "http://www.exoplatform.com/jcr/exo/1.0") ;
-      setXmlNameSpace(element, "xmlns:jcr", "http://www.jcp.org/jcr/1.0") ;
-      element.setAttribute(WORKSPACE, drive.getWorkspace()) ;
-      element.setAttribute(ICON, drive.getIcon()) ;
-      element.setAttribute(PATH, drive.getHomePath()) ;
-      element.setAttribute(SHOW_REFERENCES, Boolean.toString(drive.getViewPreferences())) ;
-      element.setAttribute(SHOW_NON_DOCUMENT, Boolean.toString(drive.getViewNonDocument())) ;
-      element.setAttribute(SHOW_SIDEBAR, Boolean.toString(drive.getViewSideBar())) ;
-      element.setAttribute(SHOW_HIDDEN_NODE, Boolean.toString(drive.getShowHiddenNode())) ;
-      element.setAttribute(ALLOW_CREATE_FOLDER, drive.getAllowCreateFolder()) ;
-      element.setAttribute(PERMISSIONS, toMultiValue(drive.getAccessPermissions())) ;
-      element.setAttribute(VIEWS, toMultiValue(drive.getViews())) ;
+      registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;      
+      mapToRegistryEntry(drive, registryEntry) ;
       registryService_.recreateEntry(sessionProvider, groupPath, registryEntry) ;
-    }catch(Exception e) {
-      registryEntry = new RegistryEntry(drive.getName()) ;
-      Element element = registryEntry.getDocument().getDocumentElement() ;
-      setXmlNameSpace(element, "xmlns:exo", "http://www.exoplatform.com/jcr/exo/1.0") ;
-      setXmlNameSpace(element, "xmlns:jcr", "http://www.jcp.org/jcr/1.0") ;
-      element.setAttribute(WORKSPACE, drive.getWorkspace()) ;
-      element.setAttribute(ICON, drive.getIcon()) ;
-      element.setAttribute(PATH, drive.getHomePath()) ;
-      element.setAttribute(SHOW_REFERENCES, Boolean.toString(drive.getViewPreferences())) ;
-      element.setAttribute(SHOW_NON_DOCUMENT, Boolean.toString(drive.getViewNonDocument())) ;
-      element.setAttribute(SHOW_SIDEBAR, Boolean.toString(drive.getViewSideBar())) ;
-      element.setAttribute(SHOW_HIDDEN_NODE, Boolean.toString(drive.getShowHiddenNode())) ;
-      element.setAttribute(ALLOW_CREATE_FOLDER, drive.getAllowCreateFolder()) ;
-      element.setAttribute(PERMISSIONS, toMultiValue(drive.getAccessPermissions())) ;
-      element.setAttribute(VIEWS, toMultiValue(drive.getViews())) ;
+    }catch(PathNotFoundException e) {
+      registryEntry = new RegistryEntry(drive.getName()) ;     
+      mapToRegistryEntry(drive, registryEntry) ;
       registryService_.createEntry(sessionProvider, groupPath, registryEntry) ;
     }
   }
@@ -101,80 +99,69 @@ public class DriveManagerServiceImpl implements DriveManagerService{
     List<DriveEntry> driveEntries = new ArrayList<DriveEntry>() ;
     Node regNode = registryService_.getRegistry(sessionProvider).getNode() ;
     Session session = regNode.getSession() ;
-    String queryStr = "select * from exo:registry where jcr:path LIKE '" + "/exo:registry/" + DRIVE_REGISTRY_PATH +"/%'" ;
+    String queryStr = "select * from exo:registryEntry where jcr:path LIKE '" + "/exo:registry/" + DRIVE_REGISTRY_PATH +"/%'" ;
+
     QueryManager queryManager = session.getWorkspace().getQueryManager() ;
     Query query = queryManager.createQuery(queryStr, Query.SQL) ;
     QueryResult result = query.execute() ;
     NodeIterator nodeIterator = result.getNodes() ;
-    System.out.println("\n\n ===========> Size of Node Iterator: "+ nodeIterator.getSize()) ;
     while (nodeIterator.hasNext()) {
       Node node = nodeIterator.nextNode() ;
-      RegistryEntry registryEntry = registryService_.getEntry(sessionProvider, node.getPath()) ;
-      Element element = registryEntry.getDocument().getDocumentElement() ;
+      String entryPath = node.getPath().substring("/exo:registry/".length()) ;
+      RegistryEntry registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;      
       DriveEntry driveEntry = new DriveEntry() ;
-      driveEntry.setWorkspace(element.getAttribute(WORKSPACE)) ;
-      driveEntry.setIcon(element.getAttribute(ICON)) ;
-      driveEntry.setHomePath(element.getAttribute(PATH)) ;
-      driveEntry.setViewPreferences(Boolean.parseBoolean(element.getAttribute(SHOW_REFERENCES))) ;
-      driveEntry.setAllowCreateFolder(element.getAttribute(ALLOW_CREATE_FOLDER)) ;
-      driveEntry.setViewNonDocument(Boolean.parseBoolean(element.getAttribute(SHOW_NON_DOCUMENT))) ;
-      driveEntry.setViewSideBar(Boolean.parseBoolean(element.getAttribute(SHOW_SIDEBAR))) ;
-      driveEntry.setShowHiddenNode(Boolean.parseBoolean(element.getAttribute(SHOW_HIDDEN_NODE)));
-      driveEntry.setAcessPermissions(fromMultiValue(element.getAttribute(PERMISSIONS))) ;
-      driveEntry.setViews(fromMultiValue(element.getAttribute(VIEWS))) ;
+      mapToDriveEntry(registryEntry, driveEntry) ;
       driveEntries.add(driveEntry) ;
     }
-    
+
     return driveEntries ;
   }
 
   public DriveEntry getDrive(String repository, String group, String name, SessionProvider sessionProvider) throws Exception {
     DriveEntry driveEntry = new DriveEntry() ;
     String entryPath = DRIVE_REGISTRY_PATH + "/" + group + "/" + name ;
-    RegistryEntry registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;    
-    //Debug code
-    Node registryNode = registryService_.getRegistry(sessionProvider).getNode();
-    System.out.println("==============>"+registryNode.getPath());
-    System.out.println("==============>"+registryNode.getPrimaryNodeType().getName());
-    Element doc = registryEntry.getDocument().getDocumentElement() ;
-    driveEntry.setWorkspace(doc.getAttribute(WORKSPACE)) ;
-    driveEntry.setIcon(doc.getAttribute(ICON)) ;
-    driveEntry.setHomePath(doc.getAttribute(PATH)) ;
-    driveEntry.setViewPreferences(Boolean.parseBoolean(doc.getAttribute(SHOW_REFERENCES))) ;
-    driveEntry.setAllowCreateFolder(doc.getAttribute(ALLOW_CREATE_FOLDER)) ;
-    driveEntry.setViewNonDocument(Boolean.parseBoolean(doc.getAttribute(SHOW_NON_DOCUMENT))) ;
-    driveEntry.setViewSideBar(Boolean.parseBoolean(doc.getAttribute(SHOW_SIDEBAR))) ;
-    driveEntry.setShowHiddenNode(Boolean.parseBoolean(doc.getAttribute(SHOW_HIDDEN_NODE)));
-    driveEntry.setAcessPermissions(fromMultiValue(doc.getAttribute(PERMISSIONS))) ;
-    driveEntry.setViews(fromMultiValue(doc.getAttribute(VIEWS))) ;
-
+    RegistryEntry registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;   
+    mapToDriveEntry(registryEntry, driveEntry) ;
     return driveEntry;
   }
 
   // should naming method's name is getDrivesByGroup
-  public List<DriveEntry> getDrives(String repository, String group, SessionProvider sessionProvider) throws Exception {
-    List<DriveEntry> driveEntriesByGroup = new ArrayList<DriveEntry>() ;
-    List<DriveEntry> allDriveEntrie = getAllDrives(repository, sessionProvider) ;
-    for (DriveEntry drive : allDriveEntrie) {
-      if (hasPermission(drive.getAccessPermissions(), group)) {
-        driveEntriesByGroup.add(drive) ;
-      }
+  public List<DriveEntry> getDrivesByGroup(String repository, String group, SessionProvider sessionProvider) throws Exception {
+    List<DriveEntry> driveEntries = new ArrayList<DriveEntry>() ;
+    Node regNode = registryService_.getRegistry(sessionProvider).getNode() ;
+    Session session = regNode.getSession() ;
+    QueryManager queryManager = session.getWorkspace().getQueryManager() ;
+    String queryStr = "select * from exo:registryEntry where jcr:path LIKE '/exo:registry/" + DRIVE_REGISTRY_PATH + "/" + group + "/%'" ;
+    Query query = queryManager.createQuery(queryStr, Query.SQL) ;
+    QueryResult result = query.execute() ;
+    NodeIterator nodeIterator = result.getNodes() ;
+    while (nodeIterator.hasNext()) {
+      Node node = nodeIterator.nextNode() ;
+      String entryPath = node.getPath().substring("/exo:registry/".length()) ;        
+      RegistryEntry  registryEntry = registryService_.getEntry(sessionProvider, entryPath) ;   
+      DriveEntry driveEntry = new DriveEntry() ;
+      mapToDriveEntry(registryEntry, driveEntry) ;
+      driveEntries.add(driveEntry) ;
     }
-
-    return driveEntriesByGroup ;
+    return driveEntries ;
   }
 
   public List<DriveEntry> getDrivesByUser(String repository, String userId, SessionProvider sessionProvider) throws Exception {
-    return null ;
+    List<DriveEntry> userDrives = new ArrayList<DriveEntry>() ;
+    for(DriveEntry driveEntry: getAllDrives(repository, sessionProvider)) {
+      if(permissionMgtService_.hasPermission(userId, driveEntry.getAccessPermissions())) {
+        userDrives.add(driveEntry) ;
+      }
+    }
+    return userDrives ;
   }
 
   public void removeDrive(String repository, String group, String name, SessionProvider sessionProvider) throws Exception {
     String entryPath = DRIVE_REGISTRY_PATH + "/" + group + "/" + name ;
     registryService_.removeEntry(sessionProvider, entryPath) ;
-    sessionProvider.close() ;
   }
 
-  private String toMultiValue(List<String> list) {
+  private String toXMLMultiValue(List<String> list) {
     int size = list.size() ;
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < size; i++) {
@@ -184,7 +171,7 @@ public class DriveManagerServiceImpl implements DriveManagerService{
     return builder.toString() ;
   }
 
-  private ArrayList<String> fromMultiValue(String attribute) {
+  private ArrayList<String> fromXMLMultiValue(String attribute) {
     ArrayList<String> list = new ArrayList<String>() ;
     for (String value : attribute.split(" ")) {
       list.add(value);
@@ -192,21 +179,67 @@ public class DriveManagerServiceImpl implements DriveManagerService{
     return list;
   }
 
-  private boolean hasPermission(ArrayList<String> permissionList, String permission) {
-    if (permission == null) return false ;
-    if (permission.indexOf(":/") > -1) {
-      String[] array = permission.split(":/") ;
-      if (array == null || array.length < 2) return false ;
-      if (permissionList.contains("*:/"+array[1])) return true ;
-    }
-
-    return permissionList.contains(permission) ;
-  }
-  
-  private void setXmlNameSpace(Element element, String key, String value) {
+  private void setXmlNamespace(Element element, String key, String value) {
     String xmlns = element.getAttribute(key) ; 
     if(xmlns == null || xmlns.trim().length() < 1) {
       element.setAttribute(key, value) ;
     }    
   }
+
+  private void mapToRegistryEntry(DriveEntry drive, RegistryEntry registryEntry) {
+    Element element = registryEntry.getDocument().getDocumentElement() ;
+    setXmlNamespace(element, "xmlns:exo", "http://www.exoplatform.com/jcr/exo/1.0") ;
+    setXmlNamespace(element, "xmlns:jcr", "http://www.jcp.org/jcr/1.0") ;
+    element.setAttribute(DRIVE_NAME, drive.getName()) ;
+    element.setAttribute(GROUP, drive.getGroup()) ;
+    element.setAttribute(REPOSITORY, drive.getRepository()) ;
+    element.setAttribute(WORKSPACE, drive.getWorkspace()) ;
+    element.setAttribute(ICON, drive.getIconPath()) ;
+    element.setAttribute(PATH, drive.getHomePath()) ;
+    element.setAttribute(SHOW_REFERENCES, Boolean.toString(drive.getShowPreferences())) ;
+    element.setAttribute(SHOW_NON_DOCUMENT, Boolean.toString(drive.getShowNonDocument())) ;
+    element.setAttribute(SHOW_SIDEBAR, Boolean.toString(drive.getShowSideBar())) ;
+    element.setAttribute(SHOW_HIDDEN_NODE, Boolean.toString(drive.getShowHiddenNode())) ;
+    element.setAttribute(ALLOW_CREATE_FOLDER, drive.getAllowCreateFolder()) ;
+    element.setAttribute(PERMISSIONS, toXMLMultiValue(drive.getAccessPermissions())) ;
+    element.setAttribute(VIEWS, toXMLMultiValue(drive.getViews())) ;
+  }
+
+  private void mapToDriveEntry(RegistryEntry registryEntry, DriveEntry drive) {
+    Element element = registryEntry.getDocument().getDocumentElement() ;
+    drive.setName(element.getAttribute(DRIVE_NAME)) ;
+    drive.setGroup(element.getAttribute(GROUP)) ;
+    drive.setRepository(element.getAttribute(REPOSITORY)) ;
+    drive.setWorkspace(element.getAttribute(WORKSPACE)) ;
+    drive.setIconPath(element.getAttribute(ICON)) ;
+    drive.setHomePath(element.getAttribute(PATH)) ;
+    drive.setShowPreferences(Boolean.parseBoolean(element.getAttribute(SHOW_REFERENCES))) ;
+    drive.setAllowCreateFolder(element.getAttribute(ALLOW_CREATE_FOLDER)) ;
+    drive.setShowNonDocument(Boolean.parseBoolean(element.getAttribute(SHOW_NON_DOCUMENT))) ;
+    drive.setShowSideBar(Boolean.parseBoolean(element.getAttribute(SHOW_SIDEBAR))) ;
+    drive.setShowHiddenNode(Boolean.parseBoolean(element.getAttribute(SHOW_HIDDEN_NODE)));
+    drive.setAcessPermissions(fromXMLMultiValue(element.getAttribute(PERMISSIONS))) ;
+    drive.setViews(fromXMLMultiValue(element.getAttribute(VIEWS))) ;
+  }
+
+  public void start() {
+    SessionProvider sessionProvider = SessionProvider.createSystemProvider() ;     
+    for(DriveManagerPlugin plugin: drivePlugins_) {
+      for(Iterator<ObjectParameter> iterator = plugin.getPredefinedDriveEntries();iterator.hasNext();) {
+        DriveEntry driveEntry = (DriveEntry)iterator.next().getObject();
+        try {
+          addDrive(driveEntry, sessionProvider) ; 
+        } catch (Exception e) {
+          log_.error("Can not init drive"+driveEntry.getName(), e) ;
+        }       
+      }
+    }
+    sessionProvider.close() ;
+
+  }
+
+  public void stop() {
+
+  }
+
 }
