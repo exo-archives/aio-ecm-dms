fdb2d77bec9c1364facba0bc916c14ba8c831e3d
Dang Tan Dung <dung.dang@exoplatform.com>
2008-05-19 08:17:37 +0000

    Dang Tan Dung commit TestTemplateService.java

--- component/ecm/src/test/java/org/exoplatform/services/ecm/template/TestTemplateService.java
+++ component/ecm/src/test/java/org/exoplatform/services/ecm/template/TestTemplateService.java
@@ -17,13 +17,22 @@
 package org.exoplatform.services.ecm.template;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.jcr.PathNotFoundException;
 
 import org.exoplatform.services.ecm.BaseECMTestCase;
 import org.exoplatform.services.jcr.ext.app.SessionProviderService;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
+import org.exoplatform.services.organization.Group;
+import org.exoplatform.services.organization.GroupHandler;
+import org.exoplatform.services.organization.MembershipHandler;
+import org.exoplatform.services.organization.MembershipType;
+import org.exoplatform.services.organization.MembershipTypeHandler;
 import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.User;
+import org.exoplatform.services.organization.UserHandler;
+import org.exoplatform.services.organization.auth.AuthenticationService;
 
 /**
  * Created by The eXo Platform SAS
@@ -34,18 +43,25 @@ import org.exoplatform.services.organization.OrganizationService;
 public class TestTemplateService extends BaseECMTestCase{
   private String GROUP1 = "group1".intern() ;
   private String GROUP2 = "group2".intern() ;
-  private String memship1 = "user1".intern() ;
-  private String memship2 = "user2".intern() ;
+  private String memshipType1 = "user1".intern() ;
+  private String memshipType2 = "user2".intern() ;
 
   private NodeTemplateService nodeTemplateService_ ;
   private ArrayList<String> permission1, permission2 ;
   private SessionProviderService sessionProviderService_ ;
   private OrganizationService organizationServices_ ;
+  private GroupHandler groupHandler_ ;
+  private MembershipTypeHandler membershipTypeHandler_ ;
+  private UserHandler userHandler_ ;
+  private MembershipHandler membershipHandler_ ;
+  private AuthenticationService authenticationService_ ;
+
 
   public void setUp() throws Exception {
     super.setUp() ;
     nodeTemplateService_ = (NodeTemplateService) container.getComponentInstanceOfType(NodeTemplateService.class) ;
     sessionProviderService_ = (SessionProviderService) container.getComponentInstanceOfType(SessionProviderService.class) ;
+    organizationServices_ = (OrganizationService) container.getComponentInstanceOfType(OrganizationService.class) ;
   }
 
   public void tearDown() {
@@ -55,12 +71,18 @@ public class TestTemplateService extends BaseECMTestCase{
   public void testAddTemplate() throws Exception {
     SessionProvider sessionProvider = sessionProviderService_.getSystemSessionProvider(null) ;
     TemplateEntry tempEntry = new TemplateEntry() ;
+    try{
+      nodeTemplateService_.getTemplate("nt:file", "exo:article", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException ex) {}
     tempEntry = createTemplateEntryToTest("nt:file", "article", "exo:article", true, true, permission1, "<abc>") ;
     assertNotNull(tempEntry) ;
     nodeTemplateService_.addTemplate(tempEntry, "repository", sessionProvider) ;
 
     TemplateEntry savedTemplate = new TemplateEntry() ;
     savedTemplate = nodeTemplateService_.getTemplate("nt:file", "exo:article", true, "repository", sessionProvider) ;
+
+    assertGetTemplate(tempEntry, savedTemplate) ;
 
     nodeTemplateService_.removeTemplate("nt:file", "exo:article", true, "repository", sessionProvider) ;
     Exception ex = new Exception();
@@ -70,12 +92,17 @@ public class TestTemplateService extends BaseECMTestCase{
       ex = e ;
     }
     assertEquals(ex.getClass(), PathNotFoundException.class) ;
-    assertGetTemplate(tempEntry, savedTemplate) ;
+
+    sessionProvider.close() ;
   }
 
   public void testGetTemplatePath() throws Exception {
     SessionProvider sessionProvider = sessionProviderService_.getSystemSessionProvider(null) ;
     TemplateEntry templateEntry = new TemplateEntry() ;
+    try {
+      nodeTemplateService_.getTemplate("xml", "news", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {}
     templateEntry = createTemplateEntryToTest("xml", "data", "news", true, false, permission1, "<abc>") ;
     String entryPath = "/exo:registry/exo:services/exo:ecm/exo:templates/"+ "xml/" +"dialog/"+ "news" ;
     nodeTemplateService_.addTemplate(templateEntry, "repository", sessionProvider) ;
@@ -84,7 +111,79 @@ public class TestTemplateService extends BaseECMTestCase{
     String savedTemplatePath = nodeTemplateService_.getTemplatePath("xml", "news", true, "repository", sessionProvider) ; 
     assertNotNull(savedTemplatePath) ;
     assertEquals(entryPath, savedTemplatePath) ;
+
     nodeTemplateService_.removeTemplate("xml", "news", true, "repository", sessionProvider) ;
+    sessionProvider.close() ;
+  }
+
+  public void testIsManagedNodeType() throws Exception {
+    SessionProvider sessionProvider = sessionProviderService_.getSystemSessionProvider(null) ;
+    TemplateEntry templateEntry = new TemplateEntry() ;
+    try {
+      nodeTemplateService_.getTemplate("xml", "news", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {}
+    templateEntry = createTemplateEntryToTest("xml", "data", "news", true, false, permission1, "<abc>") ;
+
+    nodeTemplateService_.addTemplate(templateEntry, "repository", sessionProvider) ;
+    assertEquals(true, nodeTemplateService_.isManagedNodeType("xml", "repository", sessionProvider)) ;
+
+    nodeTemplateService_.removeTemplate("xml", "news", true, "repository", sessionProvider) ;
+    sessionProvider.close() ;
+  }
+
+  public void testRemoveManagedNodeType() throws Exception {
+    SessionProvider sessionProvider = sessionProviderService_.getSystemSessionProvider(null) ;
+    TemplateEntry templateEntry = new TemplateEntry() ;
+    try {
+      nodeTemplateService_.getTemplate("xml", "news", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {}
+    templateEntry = createTemplateEntryToTest("xml", "data", "news", true, false, permission1, "<abc>") ;
+
+    nodeTemplateService_.addTemplate(templateEntry, "repository", sessionProvider) ;
+    nodeTemplateService_.removeManagedNodeType("xml", "repository", sessionProvider) ;
+    assertEquals(false, nodeTemplateService_.isManagedNodeType("xml", "repository", sessionProvider)) ;
+    Exception ex = new Exception() ;
+    try{
+      nodeTemplateService_.removeTemplate("xml", "news", true, "repository", sessionProvider) ;
+      fail("This should has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {
+      ex = e;
+    }
+    assertEquals(ex.getClass(), PathNotFoundException.class) ;
+    sessionProvider.close() ;
+  }
+
+  public void testGetDocumentNodeTypes() throws Exception {
+    SessionProvider sessionProvider = sessionProviderService_.getSystemSessionProvider(null) ;
+    TemplateEntry templateEntry1 = new TemplateEntry() ;
+    TemplateEntry templateEntry2 = new TemplateEntry() ;
+    try {
+      nodeTemplateService_.getTemplate("xml", "news1", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {}
+
+    try {
+      nodeTemplateService_.getTemplate("xml", "news2", true, "repository", sessionProvider) ;
+      fail("This shoud has been PathNotFoundException") ;
+    }catch(PathNotFoundException e) {}
+
+    templateEntry1 = createTemplateEntryToTest("xml", "label1", "news1", true, true, permission1, "<cba>") ;
+    templateEntry2 = createTemplateEntryToTest("xml", "label2", "news2", true, true, permission1, "<dfdf?134!@#$%>") ;
+    nodeTemplateService_.addTemplate(templateEntry1, "repository", sessionProvider) ;
+    nodeTemplateService_.addTemplate(templateEntry2, "repository", sessionProvider) ;
+
+    List<String> documentNodeTypeList = new ArrayList<String>() ;
+    documentNodeTypeList = nodeTemplateService_.getDocumentNodeTypes("repository", sessionProvider) ;
+    assertNotNull(documentNodeTypeList) ;
+    assertEquals(2, documentNodeTypeList.size()) ;
+    assertEquals("exo:file", documentNodeTypeList.get(0)) ;
+    assertEquals("xml", documentNodeTypeList.get(1)) ;
+
+    nodeTemplateService_.removeTemplate("xml", "news1", true, "repository", sessionProvider) ;
+    nodeTemplateService_.removeTemplate("xml", "news2", true, "repository", sessionProvider) ;
+    sessionProvider.close() ;
   }
 
   private TemplateEntry createTemplateEntryToTest(String nodeTypeName, String label, String templateName,
@@ -97,9 +196,8 @@ public class TestTemplateService extends BaseECMTestCase{
     tempEntry.setDialog(isDialog) ;
     tempEntry.setDocumentTemplate(isDocumentTemplate) ;
     permission = new ArrayList<String>() ;
-    for (int i=0; i < 3;i++) {
-      permission.add(templateName + i) ;
-    }
+    permission.add("member:/organization/management/executive-board") ;
+    permission.add("manager:/platform/administrators") ;
     tempEntry.setAccessPermissions(permission) ;
     tempEntry.setTemplateData(templateData) ;
     return tempEntry ;
@@ -113,5 +211,35 @@ public class TestTemplateService extends BaseECMTestCase{
     assertEquals(tempEntry.isDialog(), savedTemplate.isDialog()) ;
     assertEquals(tempEntry.isDocumentTemplate(), savedTemplate.isDocumentTemplate()) ;
     assertEquals(tempEntry.getAccessPermissions(), savedTemplate.getAccessPermissions()) ;
+  }
+
+  private void createPermissionToTest() throws Exception {
+    groupHandler_ = organizationServices_.getGroupHandler() ;
+    Group group1 = groupHandler_.createGroupInstance() ;
+    if (group1 == null) {System.out.println("\n\n===========> group1 null");} else {group1.setGroupName(GROUP1) ;} ;
+
+    groupHandler_.createGroup(group1, true) ;
+
+    Group group2 = groupHandler_.createGroupInstance() ;
+    group2.setGroupName(GROUP2) ;
+    groupHandler_.createGroup(group2, true) ;
+
+    membershipTypeHandler_ = organizationServices_.getMembershipTypeHandler() ;
+    MembershipType memship1 = membershipTypeHandler_.createMembershipTypeInstance() ;
+    memship1.setName(memshipType1) ;
+    membershipTypeHandler_.createMembershipType(memship1, true) ;
+
+    MembershipType memship2 = membershipTypeHandler_.createMembershipTypeInstance() ;
+    memship2.setName(memshipType2) ;
+    membershipTypeHandler_.createMembershipType(memship2, true) ;
+
+    userHandler_ = organizationServices_.getUserHandler() ;
+    User user = userHandler_.createUserInstance() ;
+    user.setUserName("jame") ;
+    userHandler_.createUser(user, true) ;
+
+    membershipHandler_ = organizationServices_.getMembershipHandler() ;
+    membershipHandler_.linkMembership(user, group1, memship1, true) ;
+    membershipHandler_.linkMembership(user, group2, memship1, true) ;
   }
 }
