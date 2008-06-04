0649a6b581286b002dc8911acec2c8253a1fb938
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-06-04 09:28:33 +0000

    fix bug ECM-2180

--- portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
+++ portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
@@ -29,7 +29,7 @@ ECMUtils.prototype.fixHeight = function(portletId) {
 	var portlet = document.getElementById(portletId) ;
 	var refElement = eXo.core.DOMUtil.findAncestorByClass(portlet, "UIApplication") ;
 	if (refElement == null) return;
-	var delta = (parseInt(refElement.style.height) - portlet.offsetHeight) || 0;
+	var delta = (parseInt(refElement.style.height) - portlet.offsetHeight);
 	var resizeObj = eXo.core.DOMUtil.findDescendantsByClass(portlet, 'div', 'UIResizableBlock') ;
 	if (resizeObj.length) {
 		for(var i = 0; i < resizeObj.length; i++) {
@@ -133,58 +133,6 @@ ECMUtils.prototype.collapseExpand = function(elemt) {
 	}
 	return true;
 };
-	var discoverEXO = function(object) {
-		removeInfoDiv();
-		if (!object) return;
-		var infoDiv = document.createElement("div") ;
-			infoDiv.setAttribute("id" , "show.EX0.0bject") ;
-			infoDiv.style.background = "black" ;
-			infoDiv.style.bottom = "0px" ;
-			infoDiv.style.color = "white" ;
-			infoDiv.style.padding = "3px" ;
-			infoDiv.style.position = "absolute" ;
-			infoDiv.style.zIndex = "9999" ;
-		var closeButton = document.createElement("div") ;
-			closeButton.setAttribute("id" , "Remove.Information.0bject") ;
-			closeButton.style.paddingBottom = "10px" ;
-			closeButton.style.textAlign = "right" ;
-			closeButton.innerHTML = "<span style='color: red; font-weight: bold; cursor: pointer'>[ X ]</span>" ;
-		var blockContent = 	document.createElement("div") ;
-			blockContent.style.background = "#848484" ;
-			blockContent.style.border = "1px solid green" ;
-			blockContent.style.height = "300px" ;
-			blockContent.style.overflow = "auto" ;
-			blockContent.style.padding = "10px" ;
-			blockContent.style.width = "400px" ;
-		var csHTML = new String() ;
-		var csObject = new String() ;
-		if (typeof object == "object") {
-			for (var exo in object) {
-				if (typeof object[exo] == "string") csObject = object[exo].replace(/</g, "&lt;") ;
-				else csObject = object[exo];
-				csHTML +=  "<span style='color: #9b1a00'>" + exo + "</span> : " + csObject + "<br />" ;
-			}
-		} else if (typeof object == "string") {
-			csHTML = object.replace(/</g, "&lt;") ;
-		} else {
-			csHTML = object.toString();
-		}
-		
-		document.body.appendChild(infoDiv) ;
-		infoDiv.appendChild(closeButton) ;
-		infoDiv.appendChild(blockContent) ;
-		blockContent.innerHTML = csHTML ;
-		
-		closeButton = document.getElementById("Remove.Information.0bject") ;
-		closeButton.childNodes[0].onclick = removeInfoDiv ;
-		
-		function removeInfoDiv() {
-		 if (document.getElementById("show.EX0.0bject")) {
-			var infoDiv = document.getElementById("show.EX0.0bject") ;
-			infoDiv.parentNode.removeChild(infoDiv) ;
-			}
-		}
-	} ;
 
 ECMUtils.prototype.filterValue = function(frmId) {
 	var form = document.getElementById(frmId) ;
