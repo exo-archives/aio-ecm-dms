5f5f1b2da73d3af4d086b67657772d510872834a
Minh Van Dang <minh.dang@exoplatform.com>
2008-06-17 12:52:36 +0000

    ECM-2204 fix for problem with i18n in workflow portlet

--- portlet/workflow/src/main/java/org/exoplatform/workflow/webui/component/controller/UIControllerManager.java
+++ portlet/workflow/src/main/java/org/exoplatform/workflow/webui/component/controller/UIControllerManager.java
@@ -31,7 +31,6 @@ import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.ComponentConfigs;
 import org.exoplatform.webui.config.annotation.EventConfig;
-import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIContainer;
 import org.exoplatform.webui.core.UIGrid;
 import org.exoplatform.webui.event.Event;
