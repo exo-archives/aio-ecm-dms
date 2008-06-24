f1858a6c55d37ca82f2321549ba5aa4c070850f4
Minh Van Dang <minh.dang@exoplatform.com>
2008-06-24 12:45:56 +0000

    Refactor publication service

---
+++ component/publication/webui/plugins/src/main/java/org/exoplatform/publication/component/UINonPublishedForm.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.publication.component;
+
+import javax.jcr.Node;
+
+import org.exoplatform.webui.config.annotation.ComponentConfig;
+import org.exoplatform.webui.core.lifecycle.UIApplicationLifecycle;
+import org.exoplatform.webui.form.UIForm;
+
+/**
+ * Created by The eXo Platform SARL
+ * Author : Dang Van Minh
+ *          minh.dang@exoplatform.com
+ * Jun 24, 2008 5:04:58 PM
+ */
+@ComponentConfig (
+    lifecycle = UIApplicationLifecycle.class,
+    template = "classpath:conf/templates/nonPublished.gtmpl"
+)
+public class UINonPublishedForm extends UIForm {
+
+  private Node node_;
+  
+  public UINonPublishedForm() throws Exception {
+      addChild(UIPublicationComponent.class, null, null);
+   }
+  
+  public void setNode(Node node) {
+     this.node_=node;
+     getChild(UIPublicationComponent.class).setNode(node);
+   }
+  
+}
