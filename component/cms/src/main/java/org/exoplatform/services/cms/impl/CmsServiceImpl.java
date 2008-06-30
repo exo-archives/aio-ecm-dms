a38eb768d5ebf8e6c06f37eddcf4df0ae4c43e9f
Minh Van Dang <minh.dang@exoplatform.com>
2008-06-30 10:14:59 +0000

    Fix for problem when process with reference property

--- component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
+++ component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
@@ -407,17 +407,14 @@ public class CmsServiceImpl implements CmsService {
           referenceNodeName = ((String)value).split(":/")[1] ;
           Session session2 = jcrService.getRepository(repositoty).getSystemSession(referenceWorksapce) ;
           if(session2.getRootNode().hasNode(referenceNodeName)) {
-            Node referenceNode = session2.getRootNode().getNode(referenceNodeName);
-            Value value2add = session2.getValueFactory().createValue(referenceNode);
+            Value value2add = session2.getValueFactory().createValue(referenceNodeName);
             node.setProperty(propertyName, value2add);          
           }else {
             node.setProperty(propertyName, session2.getValueFactory().createValue((String)value));
           }
         } else {
           if(value.toString().length() > 0 && session.getRootNode().hasNode((String) value)) {
-            Node referenceNode = session.getRootNode().getNode(value.toString());
-            Value value2add = session.getValueFactory().createValue(referenceNode);
-            node.setProperty(propertyName, value2add);
+            node.setProperty(propertyName, value.toString());
           } else {
             node.setProperty(propertyName, session.getValueFactory().createValue(value.toString()));
           }
