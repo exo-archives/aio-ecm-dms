51093c9d198437f5fca1f134dd87f77c5e20538f
Minh Van Dang <minh.dang@exoplatform.com>
2008-07-01 02:49:27 +0000

    update for problem with with property reference

--- component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
+++ component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
@@ -410,7 +410,8 @@ public class CmsServiceImpl implements CmsService {
           referenceNodeName = ((String)value).split(":/")[1] ;
           Session session2 = jcrService.getRepository(repositoty).getSystemSession(referenceWorksapce) ;
           if(session2.getRootNode().hasNode(referenceNodeName)) {
-            Value value2add = session2.getValueFactory().createValue(referenceNodeName);
+            Node referenceNode = session2.getRootNode().getNode(referenceNodeName);
+            Value value2add = session2.getValueFactory().createValue(referenceNode);
             node.setProperty(propertyName, value2add);          
           }else {
             node.setProperty(propertyName, session2.getValueFactory().createValue((String)value));
