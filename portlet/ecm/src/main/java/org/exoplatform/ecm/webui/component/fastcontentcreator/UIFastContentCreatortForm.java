dabc18f707982fa4891aa2c0864fefc4bc771f85
Minh Van Dang <minh.dang@exoplatform.com>
2008-07-02 03:14:28 +0000

    a little changes with method showcomponent to get the array mimetype

--- portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/fastcontentcreator/UIFastContentCreatortForm.java
+++ portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/fastcontentcreator/UIFastContentCreatortForm.java
@@ -254,7 +254,11 @@ public class UIFastContentCreatortForm extends DialogFormFields implements UISel
             ((UIJCRBrowser)uiComp).setFilterType(new String[] {Utils.NT_FILE}) ;
             ((UIJCRBrowser)uiComp).setIsDisable(arrParams[1], true) ;
             ((UIJCRBrowser)uiComp).setRootPath(arrParams[2]) ;
-            ((UIJCRBrowser)uiComp).setMimeTypes(new String[] {arrParams[3]}) ;
+            if(arrParams[3].indexOf(";") > -1) {
+              ((UIJCRBrowser)uiComp).setMimeTypes(arrParams[3].split(";")) ;
+            } else {
+              ((UIJCRBrowser)uiComp).setMimeTypes(new String[] {arrParams[3]}) ;
+            }
           }
         }
       }
