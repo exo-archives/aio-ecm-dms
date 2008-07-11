9aacd0bc646bbdf23f3f11ef890aaab452da92e3
Minh Van Dang <minh.dang@exoplatform.com>
2008-07-11 08:14:25 +0000

    ECM-2310 fix for problem with multilanguage service

--- component/cms/src/main/java/org/exoplatform/services/cms/i18n/impl/MultiLanguageServiceImpl.java
+++ component/cms/src/main/java/org/exoplatform/services/cms/i18n/impl/MultiLanguageServiceImpl.java
@@ -352,6 +352,18 @@ public class MultiLanguageServiceImpl implements MultiLanguageService{
           for(NodeType mixin:mixins) {
             if(!mixin.getName().equals("exo:actionable")) {
               if(newLanguageNode.canAddMixin(mixin.getName())) newLanguageNode.addMixin(mixin.getName()) ;            
+              for(PropertyDefinition def: mixin.getPropertyDefinitions()) {
+                if(!def.isProtected()) {
+                  String propName = def.getName() ;
+                  if(def.isMandatory() && !def.isAutoCreated()) {
+                    if(def.isMultiple()) {
+                      newLanguageNode.setProperty(propName,node.getProperty(propName).getValues()) ;
+                    } else {
+                      newLanguageNode.setProperty(propName,node.getProperty(propName).getValue()) ; 
+                    }
+                  }        
+                }
+              }
             }
           }
         }
