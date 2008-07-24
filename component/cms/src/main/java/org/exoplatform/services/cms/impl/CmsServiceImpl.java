c9b06e2736b3840247d9720d7aab3a213f5a5d7a
Minh Van Dang <minh.dang@exoplatform.com>
2008-07-24 09:24:42 +0000

    update

--- component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
+++ component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
@@ -219,7 +219,8 @@ public class CmsServiceImpl implements CmsService {
         JcrInputProperty inputVariable = (JcrInputProperty) jcrVariables.get(currentPath) ;
         Object value = null;
         if(inputVariable != null) value = inputVariable.getValue();
-        if(value != null &&  !propertyDef.isProtected()) {
+        if((value != null || (value == null && requiredtype == PropertyType.REFERENCE)) && 
+            !propertyDef.isProtected()) {
           processProperty(propertyName, currentNode, requiredtype, value, propertyDef.isMultiple());
         }
       }
@@ -394,7 +395,8 @@ public class CmsServiceImpl implements CmsService {
       }      
       break;
     case PropertyType.REFERENCE:      
-      if (value == null) throw new RepositoryException("null value for a reference " + requiredtype);
+//      if (value == null) throw new RepositoryException("null value for a reference " + requiredtype);
+      if(value == null) node.setProperty(propertyName, "");
       if (value instanceof Value[]) {
         node.setProperty(propertyName, (Value[]) value);
       } else if (value instanceof String) {
