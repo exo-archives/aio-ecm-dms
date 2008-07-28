d704a921c27e08a4d5122a5d911ad7a57533f52f
Minh Van Dang <minh.dang@exoplatform.com>
2008-07-28 10:56:12 +0000

    update

--- web/eXoECMSkinVista/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/eXoECMSkinVista/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,9 +1,84 @@
-import java.util.ArrayList;
 
-SkinService.addSkin("PortalSkin","Default", "/eXoResources/skin/Stylesheet.css", ServletContext);
+SkinService.addSkin(
+    "ecm/ContentBrowserPortlet",
+    "Default",
+    "/ecm/skin/webui/component/browse/DefaultStylesheet.css",
+    ServletContext
+) ;
 
-SkinService.addTheme("Simple", ["SimpleBlue","SimpleViolet","SimpleOrange","SimplePink","SimpleGreen"]);
-SkinService.addTheme("RoundConer", ["RoundConerBlue","RoundConerViolet","RoundConerOrange","RoundConerPink","RoundConerGreen"]);
-SkinService.addTheme("Shadow", ["ShadowBlue","ShadowViolet","ShadowOrange","ShadowPink","ShadowGreen"]);
-SkinService.addTheme("MacStyle", ["MacTheme","MacGray","MacGreenSteel","MacBlack"]);
-SkinService.addTheme("VistaStyle", ["VistaTheme","VistaBlue"]);
+SkinService.addSkin(
+    "ecm/ContentBrowserPortlet",
+    "Vista",
+    "/ecm/skin/webui/component/browse/VistaStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "ecm/ContentBrowserPortlet",
+    "Mac",
+    "/ecm/skin/webui/component/browse/MacStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/ECMAdminPortlet",
+    "Default",
+    "/ecm/skin/webui/component/admin/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/ECMAdminPortlet",
+    "Vista",
+    "/ecm/skin/webui/component/admin/VistaStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "ecm/ECMAdminPortlet",
+    "Mac",
+    "/ecm/skin/webui/component/admin/MacStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/FileExplorerPortlet",
+    "Default",
+    "/ecm/skin/webui/component/explorer/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/FileExplorerPortlet",
+    "Vista",
+    "/ecm/skin/webui/component/explorer/VistaStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "ecm/FileExplorerPortlet",
+    "Mac",
+    "/ecm/skin/webui/component/explorer/MacStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/FastContentCreatorPortlet",
+    "Default",
+    "/ecm/skin/webui/component/fastcontentcreator/DefaultStylesheet.css",
+    ServletContext
+) ;
+
+SkinService.addSkin(
+    "ecm/FastContentCreatorPortlet",
+    "Vista",
+    "/ecm/skin/webui/component/fastcontentcreator/VistaStylesheet.css",
+    ServletContext
+) ;
+ 
+SkinService.addSkin(
+    "ecm/FastContentCreatorPortlet",
+    "Mac",
+    "/ecm/skin/webui/component/fastcontentcreator/MacStylesheet.css",
+    ServletContext
+) ;
