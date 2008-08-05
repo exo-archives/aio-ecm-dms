cfc17263efe468e89fe7bb18bdebdbf73dce7b8d
Le Minh Hao <hao.le@exoplatform.com>
2008-08-05 01:48:40 +0000

    Documentation for NodeService API

--- component/ecm/src/main/java/org/exoplatform/services/ecm/core/NodeService.java
+++ component/ecm/src/main/java/org/exoplatform/services/ecm/core/NodeService.java
@@ -32,13 +32,66 @@ public interface NodeService {
 
   public static final String NODE = "/node" ;
 
+  /**
+   * Create a new node 
+   * 
+   * @param repository The repository specifying to create
+   * @param workspace The workspace specifying to create
+   * @param parentPath The path contain this node
+   * @param nodetype Type of node 
+   * @param jcrItemInputs all properties will be hole on it
+   * @param isNew between pick crate new or edition for node
+   * @param sessionProvider provide a session
+   * @return return the node just created
+   * @throws Exception if any error occurs
+   */
   public Node addNode(String repository, String workspace,String parentPath,String nodetype,Map<String,JcrItemInput> jcrItemInputs,boolean isNew,SessionProvider sessionProvider) throws Exception ;
-
+  /**
+   * Ceate a node on a parent node
+   * 
+   * @param parent parent node 
+   * @param nodetype type of node
+   * @param jcrItemInputs all properties will be hole on it
+   * @param isNew between pick create new or edition for node
+   * @return return the node just created
+   * @throws Exception if any error occurs
+   */
   public Node addNode(Node parent,String nodetype,Map<String,JcrItemInput> jcrItemInputs,boolean isNew) throws Exception;
-
+  /**
+   * move a node from specify a path
+   * 
+   * @param repository repository specifying
+   * @param srcWorkspace source workspace
+   * @param srcPath source path, contain source node
+   * @param destWorkspace destination workspace
+   * @param destPath destination path, contain node was move
+   * @param sessionProvider provide a session
+   * @return node was move
+   * @throws Exception if any error occurs
+   */
   public Node moveNode(String repository, String srcWorkspace,String srcPath, String destWorkspace, String destPath, SessionProvider sessionProvider) throws Exception ;
-
+  /**
+   * copy a node form specify path
+   * 
+   * @param repository repository specifying
+   * @param srcWorkspace source workspace
+   * @param srcPath source path, contain source node
+   * @param destWorkspace destination workspace
+   * @param destPath destination path, contain destination node
+   * @param sessionProvider provide a session
+   * @return node was copy
+   * @throws Exception if any error occurs
+   */
   public Node copyNode(String repository, String srcWorkspace,String srcPath, String destWorkspace, String destPath, SessionProvider sessionProvider) throws Exception ;
-  
+  /**
+   * set properties for node
+   * 
+   * @param node the node, it will be set properties
+   * @param propertyName name for property
+   * @param value value on each items property
+   * @param requiredtype require types for properties of the node
+   * @param isMultiple type specify is multiable or no
+   * @throws Exception if any error occurs
+   */
   public void setProperty(Node node, String propertyName, Object value, int requiredtype, boolean isMultiple) throws Exception ;
 }
