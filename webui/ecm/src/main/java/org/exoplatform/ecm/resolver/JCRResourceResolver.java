0c254b785fd98664e410287e085da1e3b1b2bb1f
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-08-08 05:00:19 +0000

    create new NTFileResourceResolver

--- webui/ecm/src/main/java/org/exoplatform/ecm/resolver/JCRResourceResolver.java
+++ webui/ecm/src/main/java/org/exoplatform/ecm/resolver/JCRResourceResolver.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
-package org.exoplatform.ecm.application;
+package org.exoplatform.ecm.resolver;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -34,55 +34,83 @@ import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 
 /**
- * Created by The eXo Platform SARL
- * Author : Dang Van Minh
- *          minh.dang@exoplatform.com
- * May 8, 2008 3:07:02 PM
+ * Created by The eXo Platform SARL Author : Dang Van Minh
+ * minh.dang@exoplatform.com May 8, 2008 3:07:02 PM
  */
-public class JCRResourceResolver extends ResourceResolver {  
-  private String repository_ ;
-  private String workspace_ ;  
-  private String propertyName_ ;
+public class JCRResourceResolver extends ResourceResolver {      
+  protected String repository ; 
+  protected String workspace ;      
+  protected String propertyName ;
 
+  /**
+   * Instantiates a new jCR resource resolver 
+   * to load template that stored as a property of node in jcr
+   * 
+   * @param repository the repository
+   * @param workspace the workspace
+   * @param propertyName the property name
+   */
   public JCRResourceResolver(String repository,String workspace,String propertyName) {
-    repository_ = repository ;
-    workspace_ = workspace;    
-    propertyName_ = propertyName ;
+    this.repository = repository ;
+    this.workspace = workspace;    
+    this.propertyName = propertyName ;
   }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#getResource(java.lang.String)
+   */
   @SuppressWarnings("unused")
   public URL getResource(String url) throws Exception {
     throw new Exception("This method is not  supported") ;  
   }
 
+  /** 
+   * @param url URL must be like jcr:path with path is node path 
+   * @see org.exoplatform.resolver.ResourceResolver#getInputStream(java.lang.String)
+   */
   public InputStream getInputStream(String url) throws Exception  {
     ExoContainer container = ExoContainerContext.getCurrentContainer() ;
     RepositoryService repositoryService = 
       (RepositoryService)container.getComponentInstanceOfType(RepositoryService.class) ;
-    ManageableRepository manageableRepository = repositoryService.getRepository(repository_) ;
+    ManageableRepository manageableRepository = repositoryService.getRepository(repository) ;
     //Use system session to access jcr resource
     SessionProvider provider = SessionProviderFactory.createSystemProvider();
-    Session session = provider.getSession(workspace_,manageableRepository);
+    Session session = provider.getSession(workspace,manageableRepository);
     Node node = (Node)session.getItem(removeScheme(url)) ;
-    return new ByteArrayInputStream(node.getProperty(propertyName_).getString().getBytes()) ;
+    return new ByteArrayInputStream(node.getProperty(propertyName).getString().getBytes()) ;
   }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#getResources(java.lang.String)
+   */
   @SuppressWarnings("unused")
   public List<URL> getResources(String url) throws Exception {
     throw new Exception("This method is not  supported") ;
   }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#getInputStreams(java.lang.String)
+   */
   public List<InputStream> getInputStreams(String url) throws Exception {
     ArrayList<InputStream>  inputStreams = new ArrayList<InputStream>(1) ;
     inputStreams.add(getInputStream(url)) ;
     return inputStreams ;
   }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#isModified(java.lang.String, long)
+   */
   @SuppressWarnings("unused")
   public boolean isModified(String url, long lastAccess) {  return false ; }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#createResourceId(java.lang.String)
+   */
   public String createResourceId(String url) { return url ; }
 
+  /* (non-Javadoc)
+   * @see org.exoplatform.resolver.ResourceResolver#getResourceScheme()
+   */
   public String getResourceScheme() {  return "jcr:" ; }
 
 }
