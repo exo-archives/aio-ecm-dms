2418362f94ae78e4dd69de2d5dbdd6049d4cd5fa
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-08-12 03:55:45 +0000

    change ECMResource to CoreSkin to solve css overide prb

--- web/eXoECMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/eXoECMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,3 +1,3 @@
-SkinService.addPortalSkin("ECMResources","Default", "/eXoECMResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("ECMResources","Mac", "/eXoECMResources/skin/MacSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("ECMResources","Vista", "/eXoECMResources/skin/VistaSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("CoreSkin","Default", "/eXoECMResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("CoreSkin","Mac", "/eXoECMResources/skin/MacSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("CoreSkin","Vista", "/eXoECMResources/skin/VistaSkin/Stylesheet.css", ServletContext);
