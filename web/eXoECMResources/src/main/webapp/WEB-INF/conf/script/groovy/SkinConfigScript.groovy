26ce4dcd28f1dece1dd391eb862f1527add868e0
Minh Van Dang <minh.dang@exoplatform.com>
2008-08-15 08:16:08 +0000

    Fix for problem with eXoECMResources

--- web/eXoECMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++ web/eXoECMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -1,3 +1,3 @@
-SkinService.addPortalSkin("CoreSkin","Default", "/eXoECMResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("CoreSkin","Mac", "/eXoECMResources/skin/MacSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("CoreSkin","Vista", "/eXoECMResources/skin/VistaSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("ECMSkin","Default", "/eXoECMResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("ECMSkin","Mac", "/eXoECMResources/skin/MacSkin/Stylesheet.css", ServletContext);
+SkinService.addPortalSkin("ECMSkin","Vista", "/eXoECMResources/skin/VistaSkin/Stylesheet.css", ServletContext);
