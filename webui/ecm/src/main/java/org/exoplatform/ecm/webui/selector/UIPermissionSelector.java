ea00b893f36fe7d42c3c929b2960af528610357d
Do Ngoc Anh <anh.do@exoplatform.com>
2008-08-21 07:08:21 +0000

    improve for permission selector

--- webui/ecm/src/main/java/org/exoplatform/ecm/webui/selector/UIPermissionSelector.java
+++ webui/ecm/src/main/java/org/exoplatform/ecm/webui/selector/UIPermissionSelector.java
@@ -33,45 +33,58 @@ import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.webui.organization.UIGroupMembershipSelector;
 
+// TODO: Auto-generated Javadoc
 /**
  * Created by The eXo Platform SARL Author : Dang Van Minh
  * minh.dang@exoplatform.com Nov 17, 2006
  */
-@ComponentConfigs({ 
-  @ComponentConfig(
-      template = "classpath:groovy/ecm/webui/UIMemberSelector.gtmpl",
-      events = {
+@ComponentConfigs( {
+    @ComponentConfig(
+        template = "classpath:groovy/ecm/webui/UIMemberSelector.gtmpl", 
+        events = {
           @EventConfig(listeners = UIPermissionSelector.ChangeNodeActionListener.class),
           @EventConfig(listeners = UIPermissionSelector.SelectMembershipActionListener.class),
-          @EventConfig(listeners = UIPermissionSelector.SelectPathActionListener.class)  
-      }  
-  ),
-  @ComponentConfig(
-      type = UITree.class, id = "UITreeGroupSelector",
-      template = "system:/groovy/webui/core/UITree.gtmpl",
-      events = @EventConfig(listeners = UITree.ChangeNodeActionListener.class)
-  ),
-  @ComponentConfig(
-      type = UIBreadcumbs.class, id = "BreadcumbGroupSelector",
-      template = "system:/groovy/webui/core/UIBreadcumbs.gtmpl",
-      events = @EventConfig(listeners = UIBreadcumbs.SelectPathActionListener.class)
-  )
-})
+          @EventConfig(listeners = UIPermissionSelector.SelectPathActionListener.class) 
+        }),
+      @ComponentConfig(
+          type = UITree.class, 
+          id = "UITreeGroupSelector", 
+          template = "system:/groovy/webui/core/UITree.gtmpl", 
+          events = @EventConfig(listeners = UITree.ChangeNodeActionListener.class)),
+      @ComponentConfig(
+          type = UIBreadcumbs.class, 
+          id = "BreadcumbGroupSelector", 
+          template = "system:/groovy/webui/core/UIBreadcumbs.gtmpl", 
+          events = @EventConfig(listeners = UIBreadcumbs.SelectPathActionListener.class)) 
+    }
+)
+
 public class UIPermissionSelector extends UIGroupMembershipSelector implements ComponentSelector {
 
-  final static public String defaultValue = "/admin" ;
-  private UIComponent uiComponent ;
-  private String returnFieldName = null ;  
-  private boolean isSelectedMembership;
-  private boolean isSelectedUser;
-  public boolean isUsePopup = true ;
+  /** The Constant defaultValue. */
+  final static public String defaultValue    = "/admin";
+
+  /** The ui component. */
+  private UIComponent        uiComponent;
+
+  /** The return field name. */
+  private String             returnFieldName = null;
+
+  /** The is selected membership. */
+  private boolean            isSelectedMembership;
+
+  /** The is selected user. */
+  private boolean            isSelectedUser;
+
+  /** The is use popup. */
+  public boolean             isUsePopup      = true;
 
   /**
    * Instantiates a new uI permission selector.
    * 
    * @throws Exception the exception
    */
-  public UIPermissionSelector() throws Exception {        
+  public UIPermissionSelector() throws Exception {
     changeGroup(defaultValue);
   }
 
@@ -79,9 +92,12 @@ public class UIPermissionSelector extends UIGroupMembershipSelector implements C
    * Sets the current permission.
    * 
    * @param per the new current permission
+   * 
    * @throws Exception the exception
    */
-  public void setCurrentPermission(String per) throws Exception { changeGroup(per) ; }
+  public void setCurrentPermission(String per) throws Exception {
+    changeGroup(per);
+  }
 
   /**
    * Gets the return component.
@@ -89,28 +105,33 @@ public class UIPermissionSelector extends UIGroupMembershipSelector implements C
    * @return the return component
    */
 
-  public UIComponent getSourceComponent() { return uiComponent; }
+  public UIComponent getSourceComponent() {
+    return uiComponent;
+  }
 
   /**
    * Gets the return field.
    * 
    * @return the return field
    */
-  public String getReturnField() { return returnFieldName ; }
+  public String getReturnField() {
+    return returnFieldName;
+  }
 
   /* (non-Javadoc)
    * @see org.exoplatform.ecm.webui.selector.ComponentSelector#setComponent(org.exoplatform.webui.core.UIComponent, java.lang.String[])
    */
   public void setSourceComponent(UIComponent uicomponent, String[] initParams) {
-    uiComponent = uicomponent ;
-    if(initParams == null || initParams.length == 0) return ;
-    for(int i = 0; i < initParams.length; i ++) {
-      if(initParams[i].indexOf("returnField") > -1) {
-        String[] array = initParams[i].split("=") ;
-        returnFieldName = array[1] ;
-        break ;
+    uiComponent = uicomponent;
+    if (initParams == null || initParams.length == 0)
+      return;
+    for (int i = 0; i < initParams.length; i++) {
+      if (initParams[i].indexOf("returnField") > -1) {
+        String[] array = initParams[i].split("=");
+        returnFieldName = array[1];
+        break;
       }
-      returnFieldName = initParams[0] ;
+      returnFieldName = initParams[0];
     }
   }
 
@@ -125,27 +146,30 @@ public class UIPermissionSelector extends UIGroupMembershipSelector implements C
    * 
    * @see SelectMembershipActionEvent
    */
-  static  public class SelectMembershipActionListener extends EventListener<UIPermissionSelector> {   
+  static public class SelectMembershipActionListener extends EventListener<UIPermissionSelector> {
 
     /* (non-Javadoc)
      * @see org.exoplatform.webui.event.EventListener#execute(org.exoplatform.webui.event.Event)
      */
     public void execute(Event<UIPermissionSelector> event) throws Exception {
       UIPermissionSelector uiPermissionSelector = event.getSource();
-      if(uiPermissionSelector.getCurrentGroup() == null) return ;
-      String groupId = uiPermissionSelector.getCurrentGroup().getId() ;
-      String permission = event.getRequestContext().getRequestParameter(OBJECTID) ;
-      String value = permission + ":" + groupId ;
-      String returnField = uiPermissionSelector.getReturnField() ;
-      ((UISelectable)uiPermissionSelector.getSourceComponent()).doSelect(returnField, value) ;
-      if(uiPermissionSelector.isUsePopup) {
-        UIPopupWindow uiPopup = uiPermissionSelector.getParent() ;
-        uiPopup.setShow(false) ;
-        UIComponent uicomp = uiPermissionSelector.getSourceComponent().getParent() ;
-        event.getRequestContext().addUIComponentToUpdateByAjax(uicomp) ;
-        if(!uiPopup.getId().equals("PopupComponent"))event.getRequestContext().addUIComponentToUpdateByAjax(uiPopup) ;
+      if (uiPermissionSelector.getCurrentGroup() == null)
+        return;
+      String groupId = uiPermissionSelector.getCurrentGroup().getId();
+      String permission = event.getRequestContext().getRequestParameter(OBJECTID);
+      String value = permission + ":" + groupId;
+      String returnField = uiPermissionSelector.getReturnField();
+      ((UISelectable) uiPermissionSelector.getSourceComponent()).doSelect(returnField, value);
+      if (uiPermissionSelector.isUsePopup) {
+        UIPopupWindow uiPopup = uiPermissionSelector.getParent();
+        uiPopup.setShow(false);
+        UIComponent uicomp = uiPermissionSelector.getSourceComponent().getParent();
+        event.getRequestContext().addUIComponentToUpdateByAjax(uicomp);
+        if (!uiPopup.getId().equals("PopupComponent"))
+          event.getRequestContext().addUIComponentToUpdateByAjax(uiPopup);
       } else {
-        event.getRequestContext().addUIComponentToUpdateByAjax(uiPermissionSelector.getSourceComponent()) ;
+        event.getRequestContext().addUIComponentToUpdateByAjax(
+            uiPermissionSelector.getSourceComponent());
       }
     }
   }
@@ -161,16 +185,16 @@ public class UIPermissionSelector extends UIGroupMembershipSelector implements C
    * 
    * @see ChangeNodeActionEvent
    */
-  static  public class ChangeNodeActionListener extends EventListener<UITree> {   
+  static public class ChangeNodeActionListener extends EventListener<UITree> {
 
     /* (non-Javadoc)
      * @see org.exoplatform.webui.event.EventListener#execute(org.exoplatform.webui.event.Event)
      */
-    public void execute(Event<UITree> event) throws Exception {   
-      UIPermissionSelector uiPermissionSelector = event.getSource().getParent() ;
-      String groupId = event.getRequestContext().getRequestParameter(OBJECTID)  ;
-      uiPermissionSelector.changeGroup(groupId) ;
-      event.getRequestContext().addUIComponentToUpdateByAjax(uiPermissionSelector) ;
+    public void execute(Event<UITree> event) throws Exception {
+      UIPermissionSelector uiPermissionSelector = event.getSource().getParent();
+      String groupId = event.getRequestContext().getRequestParameter(OBJECTID);
+      uiPermissionSelector.changeGroup(groupId);
+      event.getRequestContext().addUIComponentToUpdateByAjax(uiPermissionSelector);
     }
   }
 
@@ -185,50 +209,78 @@ public class UIPermissionSelector extends UIGroupMembershipSelector implements C
    * 
    * @see SelectPathActionEvent
    */
-  static  public class SelectPathActionListener extends EventListener<UIBreadcumbs> {
+  static public class SelectPathActionListener extends EventListener<UIBreadcumbs> {
 
     /* (non-Javadoc)
      * @see org.exoplatform.webui.event.EventListener#execute(org.exoplatform.webui.event.Event)
      */
     public void execute(Event<UIBreadcumbs> event) throws Exception {
-      UIBreadcumbs uiBreadcumbs = event.getSource() ;
-      UIPermissionSelector uiPermissionSelector = uiBreadcumbs.getParent() ;
-      String objectId =  event.getRequestContext().getRequestParameter(OBJECTID) ;
-      uiBreadcumbs.setSelectPath(objectId);     
-      String selectGroupId = uiBreadcumbs.getSelectLocalPath().getId() ;
-      uiPermissionSelector.changeGroup(selectGroupId) ;
-      if(uiPermissionSelector.isUsePopup) {
-        UIPopupWindow uiPopup = uiBreadcumbs.getAncestorOfType(UIPopupWindow.class) ;
+      UIBreadcumbs uiBreadcumbs = event.getSource();
+      UIPermissionSelector uiPermissionSelector = uiBreadcumbs.getParent();
+      String objectId = event.getRequestContext().getRequestParameter(OBJECTID);
+      uiBreadcumbs.setSelectPath(objectId);
+      String selectGroupId = uiBreadcumbs.getSelectLocalPath().getId();
+      uiPermissionSelector.changeGroup(selectGroupId);
+      if (uiPermissionSelector.isUsePopup) {
+        UIPopupWindow uiPopup = uiBreadcumbs.getAncestorOfType(UIPopupWindow.class);
         uiPopup.setShow(true);
       }
-      event.getRequestContext().addUIComponentToUpdateByAjax(uiPermissionSelector) ;
+      event.getRequestContext().addUIComponentToUpdateByAjax(uiPermissionSelector);
     }
-  }  
-  
+  }
+
+  /**
+   * Sets the selected user.
+   * 
+   * @param bool the new selected user
+   */
   public void setSelectedUser(boolean bool) {
     isSelectedUser = bool;
   }
-  
+
+  /**
+   * Checks if is selected user.
+   * 
+   * @return true, if is selected user
+   */
   public boolean isSelectedUser() {
     return isSelectedUser;
   }
-  
+
+  /**
+   * Sets the selected membership.
+   * 
+   * @param bool the new selected membership
+   */
   public void setSelectedMembership(boolean bool) {
     isSelectedMembership = bool;
   }
-  
+
+  /**
+   * Checks if is selected membership.
+   * 
+   * @return true, if is selected membership
+   */
   public boolean isSelectedMembership() {
     return isSelectedMembership;
   }
-  
+
+  /**
+   * Gets the users.
+   * 
+   * @return the users
+   * 
+   * @throws Exception the exception
+   */
   public List getUsers() throws Exception {
-    List children = new ArrayList() ; 
-    OrganizationService service = getApplicationComponent(OrganizationService.class) ;
-    PageList userPageList = service.getUserHandler().findUsersByGroup(this.getCurrentGroup().getId()) ;    
-    for(Object child : userPageList.getAll()){
-      children.add((User)child) ;
+    List children = new ArrayList();
+    OrganizationService service = getApplicationComponent(OrganizationService.class);
+    PageList userPageList = service.getUserHandler().findUsersByGroup(
+        this.getCurrentGroup().getId());
+    for (Object child : userPageList.getAll()) {
+      children.add((User) child);
     }
-    return children ;
+    return children;
   }
-  
+
 }
