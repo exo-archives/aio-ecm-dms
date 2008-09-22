8f2d80dafdee77da0a35e62099b98cc26d3e74ad
Ly Dinh Quang <quang.ly@exoplatform.com>
2008-09-22 04:40:19 +0000

    ECM-2490

--- portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
+++ portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
@@ -272,7 +272,10 @@ public class UIBrowseContainer extends UIContainer {
 
   public Node getNodeByPath(String nodePath) throws Exception{
     try{
-      if(wsName_ == null) return (Node)getSession().getItem(nodePath);
+      if(wsName_ == null) {
+        wsName_ = getWorkSpace();
+        return (Node)getSession().getItem(nodePath);
+      }
       return (Node)getSession(getRepository(), wsName_).getItem(nodePath);
     } catch(PathNotFoundException path) {
       return (Node)getSession(getRepository(), wsName_).getItem(rootPath_);
@@ -1336,7 +1339,7 @@ public class UIBrowseContainer extends UIContainer {
       Node selectNode = null;
       if(wsName != null) {
         selectNode = uiContainer.getNodeByPath(objectId, wsName);
-        uiContainer.wsName_ = wsName;
+        if (uiContainer.wsName_ == null) uiContainer.wsName_ = wsName;
       } else {
         selectNode = uiContainer.getNodeByPath(objectId);   
       }
