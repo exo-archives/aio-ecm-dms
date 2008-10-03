7a77f20301364076c5f39bf3524e96fa76f97f2c
Pham Xuan Hoa <hoa.pham@exoplatform.com>
2008-10-03 10:25:23 +0000

    ECM-2393 change behavior for get list of document type

--- portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIDocumentFormController.java
+++ portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIDocumentFormController.java
@@ -21,9 +21,6 @@ import java.util.Collections;
 import java.util.List;
 
 import javax.jcr.Node;
-import javax.jcr.nodetype.NodeDefinition;
-import javax.jcr.nodetype.NodeType;
-import javax.jcr.nodetype.NodeTypeManager;
 
 import org.exoplatform.ecm.webui.comparator.ItemOptionNameComparator;
 import org.exoplatform.ecm.webui.popup.UIPopupComponent;
@@ -63,7 +60,7 @@ public class UIDocumentFormController extends UIContainer implements UIPopupComp
   public void setCurrentNode(Node node) { currentNode_ = node ; }
 
   public void setRepository(String repository) { repository_ = repository ; }
-  
+
   public void initPopup(UIComponent uiComp) throws Exception {
     removeChildById("PopupComponent") ;
     UIPopupWindow uiPopup = addChild(UIPopupWindow.class, null, "PopupComponent") ;
@@ -74,84 +71,22 @@ public class UIDocumentFormController extends UIContainer implements UIPopupComp
   }
 
   public List<SelectItemOption<String>> getListFileType() throws Exception {
-    List<SelectItemOption<String>> options = new ArrayList<SelectItemOption<String>>();
-    List<String> nodeTypes = new ArrayList<String>() ;
-    UISelectDocumentForm uiSelectForm = getChild(UISelectDocumentForm.class) ;
-    UIFormSelectBox uiSelectBox = uiSelectForm.getUIFormSelectBox(UISelectDocumentForm.FIELD_SELECT) ;
-    boolean hasDefaultDoc = false ;
-    NodeTypeManager ntManager = currentNode_.getSession().getWorkspace().getNodeTypeManager() ; 
-    NodeType currentNodeType = currentNode_.getPrimaryNodeType() ; 
-    NodeDefinition[] childDefs = currentNodeType.getChildNodeDefinitions() ;
+    List<SelectItemOption<String>> options = new ArrayList<SelectItemOption<String>>();    
     TemplateService templateService = getApplicationComponent(TemplateService.class) ;
-    List templates = templateService.getDocumentTemplates(repository_) ;
-    try {
-      for(int i = 0; i < templates.size(); i ++){
-        String nodeTypeName = templates.get(i).toString() ;        
-        String label = templateService.getTemplateLabel(nodeTypeName, repository_) ;
-        NodeType nodeType = ntManager.getNodeType(nodeTypeName) ;
-        if(nodeType.isMixin()) {
-          if(!nodeTypes.contains(nodeTypeName)) {
-            options.add(new SelectItemOption<String>(label, nodeTypeName));
-            nodeTypes.add(nodeTypeName) ;
-            continue;
-          }
-        }
-        NodeType[] superTypes = nodeType.getSupertypes() ;
-        boolean isCanCreateDocument = false ;
-        for(NodeDefinition childDef : childDefs){
-          NodeType[] requiredChilds = childDef.getRequiredPrimaryTypes() ;
-          for(NodeType requiredChild : requiredChilds) {          
-            if(nodeTypeName.equals(requiredChild.getName())){            
-              isCanCreateDocument = true ;
-              break ;
-            }            
-          }
-          if(nodeTypeName.equals(childDef.getName()) || isCanCreateDocument) {
-            if(!hasDefaultDoc && nodeTypeName.equals(DEFAULT_VALUE)) {
-              defaultDocument_ = DEFAULT_VALUE ;
-              hasDefaultDoc = true ;
-            }            
-            if(!nodeTypes.contains(nodeTypeName)) {
-              options.add(new SelectItemOption<String>(label, nodeTypeName));
-              nodeTypes.add(nodeTypeName) ;
-            }
-            isCanCreateDocument = true ;          
-          }
-        }      
-        if(!isCanCreateDocument){
-          for(NodeType superType:superTypes) {
-            for(NodeDefinition childDef : childDefs){          
-              for(NodeType requiredType : childDef.getRequiredPrimaryTypes()) {              
-                if (superType.getName().equals(requiredType.getName())) {
-                  if(!hasDefaultDoc && nodeTypeName.equals(DEFAULT_VALUE)) {
-                    defaultDocument_ = DEFAULT_VALUE ;
-                    hasDefaultDoc = true ;
-                  }                  
-                  if(!nodeTypes.contains(nodeTypeName)) {
-                    options.add(new SelectItemOption<String>(label, nodeTypeName));
-                    nodeTypes.add(nodeTypeName) ;
-                  }
-                  isCanCreateDocument = true ;
-                  break;
-                }
-              }
-              if(isCanCreateDocument) break ;
-            }
-            if(isCanCreateDocument) break ;
-          }
-        }            
-      }
-      Collections.sort(options, new ItemOptionNameComparator()) ;
-      uiSelectBox.setOptions(options) ;
-      if(hasDefaultDoc) {
-        uiSelectBox.setValue(defaultDocument_);
-      } else if(options.size() > 0) {
-        defaultDocument_ = options.get(0).getValue() ;
-        uiSelectBox.setValue(defaultDocument_);
-      } 
-    } catch(Exception e) {
-//    e.printStackTrace() ;
-    }
+    List<String> acceptableContentTypes = templateService.getCreationableContentTypes(currentNode_);
+    if(acceptableContentTypes.size() == 0) return options;
+    for(String contentType: acceptableContentTypes) {
+      String label = templateService.getTemplateLabel(contentType,repository_);
+      options.add(new SelectItemOption<String>(label, contentType));
+    }    
+    Collections.sort(options, new ItemOptionNameComparator()) ;
+    if(options.size()>0) {
+      UISelectDocumentForm uiSelectForm = getChild(UISelectDocumentForm.class) ;
+      UIFormSelectBox uiSelectBox = uiSelectForm.getUIFormSelectBox(UISelectDocumentForm.FIELD_SELECT) ;
+      defaultDocument_ = options.get(0).getValue();
+      uiSelectBox.setValue(defaultDocument_);
+      uiSelectBox.setOptions(options);
+    }        
     return options ;
   }
 
