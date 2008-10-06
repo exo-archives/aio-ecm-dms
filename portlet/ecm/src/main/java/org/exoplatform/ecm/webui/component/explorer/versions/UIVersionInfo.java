7aa86e4da470a79cbff8e89925fe402beb2577b7
Minh Van Dang <minh.dang@exoplatform.com>
2008-10-06 07:12:10 +0000

    ECM-2527 Throw exception when restore version of folder afer cut nt:file of this folder

--- portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/versions/UIVersionInfo.java
+++ portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/versions/UIVersionInfo.java
@@ -21,11 +21,13 @@ import javax.jcr.ReferentialIntegrityException;
 import javax.jcr.version.Version;
 import javax.jcr.version.VersionHistory;
 
+import org.exoplatform.ecm.jcr.JCRExceptionManager;
 import org.exoplatform.ecm.jcr.UIPopupComponent;
 import org.exoplatform.ecm.jcr.model.VersionNode;
 import org.exoplatform.ecm.utils.Utils;
 import org.exoplatform.ecm.webui.component.UIPopupAction;
 import org.exoplatform.ecm.webui.component.explorer.UIJCRExplorer;
+import org.exoplatform.services.jcr.impl.storage.JCRInvalidItemStateException;
 import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
@@ -160,11 +162,22 @@ public class UIVersionInfo extends UIContainer implements UIPopupComponent {
       }
       String objectId = event.getRequestContext().getRequestParameter(OBJECTID) ;
       uiVersionInfo.curentVersion_  = uiVersionInfo.rootVersion_.findVersionNode(objectId) ;
+      UIApplication uiApp = uiVersionInfo.getAncestorOfType(UIApplication.class) ;
       if(uiVersionInfo.node_.isLocked()) {
-        String lockToken1 = Utils.getLockToken(uiVersionInfo.node_);
-        uiVersionInfo.node_.getSession().addLockToken(lockToken1) ;
+        String lockToken = Utils.getLockToken(uiVersionInfo.node_);
+        uiVersionInfo.node_.getSession().addLockToken(lockToken) ;
       }
-      uiVersionInfo.node_.restore(uiVersionInfo.curentVersion_.getVersion(), true);
+      try {
+        uiVersionInfo.node_.restore(uiVersionInfo.curentVersion_.getVersion(), true);
+      } catch(JCRInvalidItemStateException invalid) {
+        uiApp.addMessage(new ApplicationMessage("UIVersionInfo.msg.invalid-item-state", null, 
+            ApplicationMessage.WARNING)) ;
+        event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages()) ;
+        return ;
+      } catch(Exception e) {
+        JCRExceptionManager.process(uiApp, e);
+        return;
+      }
       Node node = uiVersionInfo.getCurrentNode() ;
       if(!node.isCheckedOut()) node.checkout() ;
       uiExplorer.getSession().save() ;
