e2847af20fb3b0b6995097181b614f523c462a7a
Minh Van Dang <minh.dang@exoplatform.com>
2008-10-08 07:08:03 +0000

    Fix for problem with workflow

--- portlet/workflow/src/main/java/org/exoplatform/workflow/webui/component/controller/UITask.java
+++ portlet/workflow/src/main/java/org/exoplatform/workflow/webui/component/controller/UITask.java
@@ -403,8 +403,6 @@ public class UITask extends UIForm {
         if (objectId.equals(name)) {
           String transition = (String) attributes.get("transition");
           try {
-            Map variables = maps.getWorkflowVariables();
-            uiTask.serviceContainer.endTask(uiTask.identification_, variables, transition);
             if(nodePath != null) {
               Session session = 
                 SessionsUtils.getSessionProvider().getSession(srcWorkspace, repositoryService.getRepository(repository));
@@ -423,10 +421,12 @@ public class UITask extends UIForm {
               }
               session.logout();
             }
+            Map variables = maps.getWorkflowVariables();
+            uiTask.serviceContainer.endTask(uiTask.identification_, variables, transition);
             uiTask.getAncestorOfType(UIWorkflowPopup.class).deActivate() ;
             return;
           } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
           }
         }
       }
