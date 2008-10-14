e804a66e43b8637c4243c5aac64bb9511274592d
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-10-14 09:35:34 +0000

    remove file JCREvent.js

--- portlet/ecm/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ portlet/ecm/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,3 +1,5 @@
 /* ECM libraries */
 JavascriptService.addJavascript("eXo.ecm.ECMUtils", "/javascript/eXo/ecm/ECMUtils.js", ServletContext);
-JavascriptService.addJavascript("eXo.ecm.JCREvent", "/javascript/eXo/ecm/JCREvent.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UIListView", "/javascript/eXo/ecm/UIListView.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UISimpleView", "/javascript/eXo/ecm/UISimpleView.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UITreeView", "/javascript/eXo/ecm/UITreeView.js", ServletContext);
