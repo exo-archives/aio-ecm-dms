2b6d408c993bfbd9cdb8e3569c5cf507e4c84353
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2008-10-15 02:08:57 +0000

    ECM-2505 Unknown error with publication management while in check in status

--- component/publication/src/main/java/org/exoplatform/services/ecm/publication/plugins/staticdirect/UIPublicationForm.java
+++ component/publication/src/main/java/org/exoplatform/services/ecm/publication/plugins/staticdirect/UIPublicationForm.java
@@ -26,9 +26,11 @@ import javax.jcr.Value;
 
 import org.exoplatform.services.ecm.publication.PublicationService;
 import org.exoplatform.services.ecm.publication.plugins.webui.VersionNode;
+import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.web.application.RequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIApplication;
 import org.exoplatform.webui.core.UIPopupWindow;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.core.model.SelectItemOption;
@@ -56,46 +58,46 @@ public class UIPublicationForm extends UIForm {
   final static public String VISIBILITY = "visibility".intern();
   final static public String STATE = "state".intern();
   
-  private VersionNode curentVersion_ ;
-  private VersionNode rootVersion_ ;
-  private Node currentNode_ ;
-  private String visibility_ ;
-  private String state_ ;
+  private VersionNode curentVersion_;
+  private VersionNode rootVersion_;
+  private Node currentNode_;
+  private String visibility_;
+  private String state_;
   
   public UIPublicationForm() throws Exception {
   }
   
   public void updateForm(VersionNode versionNode) throws Exception {
-    String state = getStateByVersion(versionNode) ;
-    resetCurrentState(state, visibility_) ;
+    String state = getStateByVersion(versionNode);
+    resetCurrentState(state, visibility_);
   }
   
   public void resetCurrentState(String state, String visibility) throws Exception {
-    RequestContext context = RequestContext.getCurrentInstance() ;
-    ResourceBundle res = context.getApplicationResourceBundle() ;
-    String published = res.getString("UIPublicationForm.label.published") ;
-    String non_published = res.getString("UIPublicationForm.label.non-published") ;
-    String lblPublic = res.getString("UIPublicationForm.label.public") ;
-    String lblPrivate = res.getString("UIPublicationForm.label.private") ;
-    removeChildById(VISIBILITY) ;
-    removeChildById(STATE) ;
-    List<SelectItemOption<String>> visibilityOptions = new ArrayList<SelectItemOption<String>>() ;
-    visibilityOptions.add(new SelectItemOption<String>(lblPublic, StaticAndDirectPublicationPlugin.PUBLIC)) ;
-    visibilityOptions.add(new SelectItemOption<String>(lblPrivate, StaticAndDirectPublicationPlugin.PRIVATE)) ;
+    RequestContext context = RequestContext.getCurrentInstance();
+    ResourceBundle res = context.getApplicationResourceBundle();
+    String published = res.getString("UIPublicationForm.label.published");
+    String non_published = res.getString("UIPublicationForm.label.non-published");
+    String lblPublic = res.getString("UIPublicationForm.label.public");
+    String lblPrivate = res.getString("UIPublicationForm.label.private");
+    removeChildById(VISIBILITY);
+    removeChildById(STATE);
+    List<SelectItemOption<String>> visibilityOptions = new ArrayList<SelectItemOption<String>>();
+    visibilityOptions.add(new SelectItemOption<String>(lblPublic, StaticAndDirectPublicationPlugin.PUBLIC));
+    visibilityOptions.add(new SelectItemOption<String>(lblPrivate, StaticAndDirectPublicationPlugin.PRIVATE));
     addUIFormInput(new UIFormRadioBoxInput(VISIBILITY, visibility, visibilityOptions).
-        setAlign(UIFormRadioBoxInput.HORIZONTAL_ALIGN)) ;
+        setAlign(UIFormRadioBoxInput.HORIZONTAL_ALIGN));
     
-    List<SelectItemOption<String>> stateOptions = new ArrayList<SelectItemOption<String>>() ;
-    stateOptions.add(new SelectItemOption<String>(published, StaticAndDirectPublicationPlugin.PUBLISHED)) ;
-    stateOptions.add(new SelectItemOption<String>(non_published, StaticAndDirectPublicationPlugin.NON_PUBLISHED)) ;
+    List<SelectItemOption<String>> stateOptions = new ArrayList<SelectItemOption<String>>();
+    stateOptions.add(new SelectItemOption<String>(published, StaticAndDirectPublicationPlugin.PUBLISHED));
+    stateOptions.add(new SelectItemOption<String>(non_published, StaticAndDirectPublicationPlugin.NON_PUBLISHED));
     addUIFormInput(new UIFormRadioBoxInput(STATE, state, stateOptions).
-        setAlign(UIFormRadioBoxInput.HORIZONTAL_ALIGN)) ;
+        setAlign(UIFormRadioBoxInput.HORIZONTAL_ALIGN));
   }
   
   public String getStateByVersion(VersionNode versionNode) throws Exception {
-    Value[] publicationStates =  currentNode_.getProperty(StaticAndDirectPublicationPlugin.VERSIONS_PUBLICATION_STATES).getValues() ;
+    Value[] publicationStates =  currentNode_.getProperty(StaticAndDirectPublicationPlugin.VERSIONS_PUBLICATION_STATES).getValues();
     for(Value value : publicationStates) {
-      String[] arrPublicationState = value.getString().split(",") ;
+      String[] arrPublicationState = value.getString().split(",");
       for(int i=0; i < arrPublicationState.length; i++) {
         if(arrPublicationState[0].equals(versionNode.getVersion().getUUID())) {
           return arrPublicationState[1];
@@ -109,39 +111,45 @@ public class UIPublicationForm extends UIForm {
     currentNode_ = currentNode;   
     rootVersion_ = new VersionNode(currentNode_.getVersionHistory().getRootVersion());
     curentVersion_ = new VersionNode(currentNode_.getBaseVersion());
-    visibility_ = currentNode_.getProperty(StaticAndDirectPublicationPlugin.VISIBILITY).getString() ;
+    visibility_ = currentNode_.getProperty(StaticAndDirectPublicationPlugin.VISIBILITY).getString();
     
-    state_ = getStateByVersion(curentVersion_) ;
-    resetCurrentState(state_, visibility_) ;
+    state_ = getStateByVersion(curentVersion_);
+    resetCurrentState(state_, visibility_);
   }
   
   public void setVersionNode(VersionNode versionNode) {
-    curentVersion_ = versionNode ;
+    curentVersion_ = versionNode;
   }
   
   static public class SaveActionListener extends EventListener<UIPublicationForm> {
     public void execute(Event<UIPublicationForm> event) throws Exception {
-      UIPublicationForm uiForm = event.getSource() ;
-      String visibility = uiForm.<UIFormRadioBoxInput>getUIInput(VISIBILITY).getValue() ;
-      String state = uiForm.<UIFormRadioBoxInput>getUIInput(STATE).getValue() ;
+      UIPublicationForm uiForm = event.getSource();
+      String visibility = uiForm.<UIFormRadioBoxInput>getUIInput(VISIBILITY).getValue();
+      String state = uiForm.<UIFormRadioBoxInput>getUIInput(STATE).getValue();
+      UIApplication uiApp = uiForm.getAncestorOfType(UIApplication.class);
+      if(!uiForm.currentNode_.isCheckedOut()) {        
+        uiApp.addMessage(new ApplicationMessage("UIActionBar.msg.node-checkedin", null, 
+            ApplicationMessage.WARNING));
+        event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+        return;
+      }
       PublicationService publicationService = uiForm.getApplicationComponent(PublicationService.class);
       HashMap<String,String> context = new HashMap<String,String>();
       context.put("nodeVersionUUID", uiForm.curentVersion_.getVersion().getUUID());
       context.put("visibility", visibility);
-      publicationService.changeState(uiForm.currentNode_, state, context) ;
-      UIPopupWindow uiPopup = uiForm.getAncestorOfType(UIPopupWindow.class) ;
-      uiPopup.setRendered(false) ;
-      uiPopup.setShow(false) ;
+      publicationService.changeState(uiForm.currentNode_, state, context);
+      UIPopupWindow uiPopup = uiForm.getAncestorOfType(UIPopupWindow.class);
+      uiPopup.setRendered(false);
+      uiPopup.setShow(false);
     }
   }
   
   static public class CancelActionListener extends EventListener<UIPublicationForm> {
     public void execute(Event<UIPublicationForm> event) throws Exception {
       UIPublicationForm uiForm = event.getSource();
-      UIPopupWindow uiPopup = uiForm.getAncestorOfType(UIPopupWindow.class) ;
-      uiPopup.setRendered(false) ;
-      uiPopup.setShow(false) ;
+      UIPopupWindow uiPopup = uiForm.getAncestorOfType(UIPopupWindow.class);
+      uiPopup.setRendered(false);
+      uiPopup.setShow(false);
     }
   }
-  
 }
