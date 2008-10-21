ed7742e549196d3080a6630830a8beb8e06b3c54
Nguyen Hong Minh <minh.nguyen@exoplatform.com>
2008-10-21 08:44:19 +0000

    improve dragdrop in JCR file Explorer

--- portlet/ecm/src/main/webapp/javascript/eXo/ecm/UISimpleView.js
+++ portlet/ecm/src/main/webapp/javascript/eXo/ecm/UISimpleView.js
@@ -69,7 +69,7 @@ var SimpleView = function() {
 		} else {
 			// init drag drop;
 			document.onmousemove = Self.dragItemsSelected;
-			//document.onmouseup = Self.dropOutActionArea;
+			document.onmouseup = Self.dropOutActionArea;
 			
 			var itemSelected = element.cloneNode(true);
 			Self.itemsSelected = new Array(itemSelected);
