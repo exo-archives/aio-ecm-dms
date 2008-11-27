81e84191f7946451dc7d1b48df5d3eacb12692d0
Minh Van Dang <minh.dang@exoplatform.com>
2008-11-27 09:02:38 +0000

    ECM-2833 add skin config for banner portlet

---
+++ web/ecmportal/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
@@ -0,0 +1,24 @@
+/***************************BannerPortlet**************************************************/
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Default",
+    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
+) ;
+
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Vista",
+    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
+) ;
+ 
+SkinService.addSkin(
+    "web/BannerPortlet",
+    "Mac",
+    "/portal/templates/skin/webui/component/UIBannerPortlet/DefaultStylesheet.css",
+    ServletContext,
+    true
+) ;
