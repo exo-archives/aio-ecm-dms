27b9673512544edfcb5e420af13385c8517dd774
Hoang Van Hung <hung.van@exoplatform.com>
2009-01-14 09:12:50 +0000

    ECM-3000 Split ECM to 3 folders

--- web/eXoECMResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++
@@ -1,3 +0,0 @@
-SkinService.addPortalSkin("ECMSkin","Default", "/eXoECMResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("ECMSkin","Mac", "/eXoECMResources/skin/MacSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("ECMSkin","Vista", "/eXoECMResources/skin/VistaSkin/Stylesheet.css", ServletContext);
