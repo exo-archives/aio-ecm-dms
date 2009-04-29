ce7297b540f821d68f16c3cb688232f46e2211f0
Hoang Van Hung <hung.van@exoplatform.com>
2009-04-29 07:43:56 +0000

    ECM-3554 Unknown error when rename for node with "New name" field is blank

--- core/portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIRenameForm.java
+++ core/portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/popup/actions/UIRenameForm.java
@@ -31,8 +31,6 @@ import javax.jcr.version.VersionException;
 
 import org.apache.commons.logging.Log;
 import org.exoplatform.ecm.webui.component.explorer.UIJCRExplorer;
-import org.exoplatform.ecm.webui.form.validator.ECMNameValidator;
-import org.exoplatform.webui.core.UIPopupComponent;
 import org.exoplatform.ecm.webui.utils.JCRExceptionManager;
 import org.exoplatform.ecm.webui.utils.LockUtil;
 import org.exoplatform.ecm.webui.utils.Utils;
@@ -42,6 +40,7 @@ import org.exoplatform.web.application.ApplicationMessage;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
 import org.exoplatform.webui.core.UIApplication;
+import org.exoplatform.webui.core.UIPopupComponent;
 import org.exoplatform.webui.core.lifecycle.UIFormLifecycle;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
