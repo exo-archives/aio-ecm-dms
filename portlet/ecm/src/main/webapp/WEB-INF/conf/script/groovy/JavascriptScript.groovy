1a31ebcae8d41f4aa898b5c37f7207403e4f4fdc
Hoang Van Hung <hung.van@exoplatform.com>
2009-05-07 10:40:27 +0000

    ECM-3616 search node doesnt work after node is locked-unlocked: Create patch 2.3.1.1

---
+++ portlet/ecm/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -0,0 +1,5 @@
+/* ECM libraries */
+JavascriptService.addJavascript("eXo.ecm.ECMUtils", "/javascript/eXo/ecm/ECMUtils.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UIListView", "/javascript/eXo/ecm/UIListView.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UISimpleView", "/javascript/eXo/ecm/UISimpleView.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.UICoverFlow", "/javascript/eXo/ecm/UICoverFlow.js", ServletContext);
