bd219e828b6003fca6565e85dd233c921afc5ffc
Minh Van Dang <minh.dang@exoplatform.com>
2009-06-29 07:55:05 +0000

    ECM-3902 remove URLEncoder when download file name - DMS 2.3.x branches

--- core/portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/UIDocumentInfo.java
+++ core/portlet/ecm/src/main/java/org/exoplatform/ecm/webui/component/explorer/UIDocumentInfo.java
@@ -18,7 +18,6 @@ package org.exoplatform.ecm.webui.component.explorer;
 
 import java.awt.Image;
 import java.io.InputStream;
-import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -231,7 +230,7 @@ public class UIDocumentInfo extends UIContainer implements NodePresentation {
     if(fileName.lastIndexOf("."+ext)<0){
       fileName = fileName + "." +ext ;
     } 
-    dresource.setDownloadName(URLEncoder.encode(fileName, "utf-8")) ;
+    dresource.setDownloadName(fileName) ;
     return dservice.getDownloadLink(dservice.addDownloadResource(dresource)) ;
   }
 
