45479afbc7cc6fe44860781ec5ecb3130560c3bd
Ly Dinh Quang <quang.ly@exoplatform.com>
2009-06-30 10:12:35 +0000

    ECM-3691 Can not access CB after renamed the Categories path

--- core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
+++ core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
@@ -1120,7 +1120,6 @@ public class UIBrowseContainer extends UIContainer {
       setSelectedTabPath(categoryPath);
       setCurrentNodePath(categoryPath);
     } catch (Exception e) {
-      LOG.error("An error occured while loading the portlet config", e);
       setRootPath(null);
       setCategoryPath(null);
       setSelectedTabPath(null);
