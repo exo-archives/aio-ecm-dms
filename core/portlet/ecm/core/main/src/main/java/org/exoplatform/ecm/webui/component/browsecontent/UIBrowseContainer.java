c7b84ed1ec673a975f98f053092d27e87dda1bf8
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2009-06-30 08:51:13 +0000

    ECM-3750 Exception when view content of document (using tag) in Content Browser in special case

--- core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
+++ core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/browsecontent/UIBrowseContainer.java
@@ -22,6 +22,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -1306,6 +1307,7 @@ public class UIBrowseContainer extends UIContainer {
   }
   
   public List<Node> getListHistoryNode() {
+    listHistoryNode.remove(null);
     return listHistoryNode; 
   }
   
