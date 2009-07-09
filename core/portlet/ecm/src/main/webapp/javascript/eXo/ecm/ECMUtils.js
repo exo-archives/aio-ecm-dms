f07df79bd1e790d3454a4223860979d8ca03f6e1
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2009-07-09 04:31:31 +0000

    ECM-3920 Errors popup appears when opening the private/public drives on webos on ie6,7 after fresh login

--- core/portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
+++ core/portlet/ecm/src/main/webapp/javascript/eXo/ecm/ECMUtils.js
@@ -8,7 +8,6 @@
 	
 	ECMUtils.prototype.popupArray = [];
 	
-	
 	ECMUtils.prototype.init = function(portletId) {
 		var portlet = document.getElementById(portletId) ;
 		if(!portlet) return ;
@@ -17,8 +16,7 @@
 			eXo.ecm.ECMUtils.closeAllPopup() ;
 		}
 		if(document.getElementById("UIPageDesktop")) {
-			Self.fixHeight(portletId);
-			return;
+			Self.fixHeight(portletId) ;
 			var uiPageDeskTop = document.getElementById("UIPageDesktop");
 			var uiJCRExplorers = DOM.findDescendantsByClass(uiPageDeskTop, 'div', 'UIJCRExplorer') ;
 			if (uiJCRExplorers.length) {
@@ -39,27 +37,27 @@
 	};
 	
 	ECMUtils.prototype.fixHeight = function(portletId) {
-       var portlet = document.getElementById(portletId);
-       var refElement = DOM.findAncestorByClass(portlet, "UIApplication");
-       if (!refElement) return;
-        
-       // 30/06/2009 
-       //Recalculate height of UIResizableBlock in the UISideBarContainer 
-       //var delta = parseInt(refElement.style.height) - portlet.offsetHeight;
-                
-       var uiControl = document.getElementById('UIControl');
-       var uiSideBar = document.getElementById('UISideBar');
-       if(!uiControl || !uiSideBar) return;
-       var uiSideBarControl = DOM.findFirstDescendantByClass(uiSideBar, 'div', 'UISideBarControl');
-       if(!uiSideBarControl) return;
-       var deltaH = refElement.offsetHeight - uiControl.offsetHeight - uiSideBarControl.offsetHeight;
-       var resizeObj = DOM.findDescendantsByClass(portlet, 'div', 'UIResizableBlock');
-       if(resizeObj.length) {
-       		for(var i = 0; i < resizeObj.length; i++) {
-          	    resizeObj[i].style.display = 'block';
-                resizeObj[i].style.height = (resizeObj[i].offsetHeight + deltaH) + "px";
-          }
-       }
+		var portlet = document.getElementById(portletId);
+ 	 	var refElement = DOM.findAncestorByClass(portlet, "UIApplication");
+ 	 	if (!refElement) return;
+ 	 	
+ 	 	// 30/06/2009
+ 	 	//Recalculate height of UIResizableBlock in the UISideBarContainer
+ 	 	//var delta = parseInt(refElement.style.height) - portlet.offsetHeight;
+ 	 		               
+ 	 	var uiControl = document.getElementById('UIControl');
+ 	 	var uiSideBar = document.getElementById('UISideBar');
+ 	 	if(!uiControl || !uiSideBar) return;
+ 	 	var uiSideBarControl = DOM.findFirstDescendantByClass(uiSideBar, 'div', 'UISideBarControl');
+ 	 	if(!uiSideBarControl) return;
+ 	 	var deltaH = refElement.offsetHeight - uiControl.offsetHeight - uiSideBarControl.offsetHeight;
+ 	 	var resizeObj = DOM.findDescendantsByClass(portlet, 'div', 'UIResizableBlock');
+ 	 	if(resizeObj.length) {
+	 	 	for(var i = 0; i < resizeObj.length; i++) {
+	 	 	  resizeObj[i].style.display = 'block';
+	 	 	  resizeObj[i].style.height = (resizeObj[i].offsetHeight + deltaH) + "px";
+	 	 	}
+ 	 	}
 	};
 	
 	ECMUtils.prototype.controlLayout = function(portletId) {
@@ -288,12 +286,12 @@
 		        path += encodeURIComponent(nodePath[i]) + "/";
 		      }
 		    }
-		    window.location = serverInfo+ "/"+portalName + "/rest/private/lnkproducer/openit.lnk?path=/"+repository +"/" +workspace + path;
+		    window.open(serverInfo + "/" + portalName + "/rest/private/lnkproducer/openit.lnk?path=/" + repository + "/" + workspace + path, '_new');
 	   	} else {
-	 	  	window.location = serverInfo + "/"+portalName + "/rest/private/jcr/"+repository +"/" +workspace + nodePath; 		 		
+	 	  	window.open(serverInfo + "/" + portalName + "/rest/private/jcr/" + repository + "/" +workspace + nodePath, '_new');
 	 	  } 	  
 	  } else {
-	    window.location = serverInfo+ "/"+portalName + "/rest/private/jcr/"+repository +"/" +workspace + nodePath;
+		  window.open(serverInfo+ "/" + portalName + "/rest/private/jcr/" + repository + "/" + workspace + nodePath, '_new');
 	  } 
 	} ;
 	
