80303982dff41d11d8e4e5d51a0f1f596ef58e0b
Lam Tuan Nguyen <lam.nguyen@exoplatform.com>
2009-07-13 08:14:23 +0000

    ECM-3938, fix select node using mouse

--- core/portlet/ecm/core/web/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ core/portlet/ecm/core/web/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,6 +1,6 @@
 /* ECM libraries */
 JavascriptService.addJavascript("eXo.ecm.ECMUtils", "/javascript/eXo/ecm/ECMUtils.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.DMSBrowser", "/javascript/eXo/ecm/DMSBrowser.js", ServletContext);
 JavascriptService.addJavascript("eXo.ecm.UIListView", "/javascript/eXo/ecm/UIListView.js", ServletContext);
 JavascriptService.addJavascript("eXo.ecm.UISimpleView", "/javascript/eXo/ecm/UISimpleView.js", ServletContext);
 JavascriptService.addJavascript("eXo.ecm.UICoverFlow", "/javascript/eXo/ecm/UICoverFlow.js", ServletContext);
-JavascriptService.addJavascript("eXo.ecm.UIFormGeneratorPortlet", "/javascript/eXo/ecm/UIFormGeneratorPortlet.js", ServletContext);
