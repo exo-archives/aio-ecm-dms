295daf85b64ab9d67dc839539090e5cb79d56cf7
Hoang Van Hung <hung.van@exoplatform.com>
2009-07-23 10:11:58 +0000

    ECM-4067 Copy to clipboard

--- core/portlet/ecm/core/web/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ core/portlet/ecm/core/web/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -4,3 +4,4 @@ JavascriptService.addJavascript("eXo.ecm.DMSBrowser", "/javascript/eXo/ecm/DMSBr
 JavascriptService.addJavascript("eXo.ecm.UIListView", "/javascript/eXo/ecm/UIListView.js", ServletContext);
 JavascriptService.addJavascript("eXo.ecm.UISimpleView", "/javascript/eXo/ecm/UISimpleView.js", ServletContext);
 JavascriptService.addJavascript("eXo.ecm.UICoverFlow", "/javascript/eXo/ecm/UICoverFlow.js", ServletContext);
+JavascriptService.addJavascript("eXo.ecm.ZeroClipboard", "/javascript/eXo/ecm/ZeroClipboard.js", ServletContext);
