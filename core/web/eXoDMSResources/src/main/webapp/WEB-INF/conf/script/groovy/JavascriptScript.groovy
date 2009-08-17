ce27f9584e1c9e4fab8adffb9b1573fc58913738
Minh Van Dang <minh.dang@exoplatform.com>
2009-08-17 08:26:21 +0000

    ECM-4180 support enlarge when click to view image of nt:file

--- core/web/eXoDMSResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
+++ core/web/eXoDMSResources/src/main/webapp/WEB-INF/conf/script/groovy/JavascriptScript.groovy
@@ -1,1 +1,5 @@
 /* ECM libraries */
+JavascriptService.addJavascript("lightbox.prototype", "/javascript/lightbox/prototype.js", ServletContext);
+JavascriptService.addJavascript("lightbox.effects", "/javascript/lightbox/effects.js", ServletContext);
+JavascriptService.addJavascript("lightbox.scriptaculous", "/javascript/lightbox/scriptaculous.js", ServletContext);
+JavascriptService.addJavascript("lightbox.lightbox", "/javascript/lightbox/lightbox.js", ServletContext);
