22a12e8642de612db21cacd6b6c2939e0377dcb3
Minh Van Dang <minh.dang@exoplatform.com>
2009-08-27 09:12:22 +0000

    ECM-4076 add method getUIComponent in ext

--- ext/contentvalidation/component/webui/src/main/java/org/exoplatform/contentvalidation/webui/UIDocumentContent.java
+++ ext/contentvalidation/component/webui/src/main/java/org/exoplatform/contentvalidation/webui/UIDocumentContent.java
@@ -18,7 +18,9 @@ package org.exoplatform.contentvalidation.webui;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
@@ -48,10 +50,13 @@ import org.exoplatform.webui.application.WebuiRequestContext;
 import org.exoplatform.webui.application.portlet.PortletRequestContext;
 import org.exoplatform.webui.config.annotation.ComponentConfig;
 import org.exoplatform.webui.config.annotation.EventConfig;
+import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.core.UIContainer;
 import org.exoplatform.webui.event.Event;
 import org.exoplatform.webui.event.EventListener;
 import org.exoplatform.workflow.webui.component.controller.UITaskManager;
+import org.exoplatform.webui.ext.UIExtension;
+import org.exoplatform.webui.ext.UIExtensionManager;
 
 /**
  * Created by The eXo Platform SARL
@@ -220,6 +225,18 @@ public class UIDocumentContent extends UIContainer {
       e.printStackTrace() ;
     }
     return nodeTypes ;
+  }
+  
+  public UIComponent getUIComponent(String mimeType) throws Exception {
+    UIExtensionManager manager = getApplicationComponent(UIExtensionManager.class);
+    List<UIExtension> extensions = manager.getUIExtensions(Utils.FILE_VIEWER_EXTENSION_TYPE);
+    Map<String, Object> context = new HashMap<String, Object>();
+    context.put(Utils.MIME_TYPE, mimeType);
+    for (UIExtension extension : extensions) {
+      UIComponent uiComponent = manager.addUIExtension(extension, context, this);
+      if(uiComponent != null) return uiComponent;
+    }
+    return null;
   }
   
   public List<Node> getAttachments() throws Exception {
