ba0a86cbc4a43cc77be0f575339f574b495aa106
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2009-09-09 03:18:31 +0000

    ECM-4148 Select category form is not shown for normal user when add new document

--- core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/upload/UIUploadForm.java
+++ core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/upload/UIUploadForm.java
@@ -542,47 +542,59 @@ public class UIUploadForm extends UIForm implements UIPopupComponent, UISelectab
     public void execute(Event<UIFormMultiValueInputSet> event) throws Exception {
       UIFormMultiValueInputSet uiSet = event.getSource();
       UIUploadForm uiUploadForm =  (UIUploadForm) uiSet.getParent();
-      UIFormUploadInput uiFormUploadInput = uiUploadForm.getChildById(FIELD_UPLOAD);
-      UploadResource uploadResource = uiFormUploadInput.getUploadResource();
-      if (uploadResource == null) {
-        UIApplication uiApp = uiUploadForm.getAncestorOfType(UIApplication.class);
-        uiApp.addMessage(new ApplicationMessage("UIUploadForm.msg.upload-not-null", null,
+      UIApplication uiApp = uiUploadForm.getAncestorOfType(UIApplication.class);
+      try {
+        UIFormUploadInput uiFormUploadInput = uiUploadForm.getChildById(FIELD_UPLOAD);
+        UploadResource uploadResource = uiFormUploadInput.getUploadResource();
+        if (uploadResource == null) {
+          uiApp.addMessage(new ApplicationMessage("UIUploadForm.msg.upload-not-null", null,
+              ApplicationMessage.WARNING));
+          event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+          return;
+        }
+        UIUploadManager uiUploadManager = uiUploadForm.getParent();
+        UIJCRExplorer uiExplorer = uiUploadForm.getAncestorOfType(UIJCRExplorer.class);
+        NodeHierarchyCreator nodeHierarchyCreator = 
+          uiUploadForm.getApplicationComponent(NodeHierarchyCreator.class);
+        String repository = uiExplorer.getRepositoryName();
+        DMSConfiguration dmsConfig = uiUploadForm.getApplicationComponent(DMSConfiguration.class);
+        DMSRepositoryConfiguration dmsRepoConfig = dmsConfig.getConfig(repository);
+        String workspaceName = dmsRepoConfig.getSystemWorkspace();
+        
+        UIPopupWindow uiPopupWindow = uiUploadManager.initPopupTaxonomy(POPUP_TAXONOMY);
+        UIOneTaxonomySelector uiOneTaxonomySelector = 
+          uiUploadManager.createUIComponent(UIOneTaxonomySelector.class, null, null);
+        uiPopupWindow.setUIComponent(uiOneTaxonomySelector);
+        uiOneTaxonomySelector.setIsDisable(workspaceName, false);
+        String rootTreePath = nodeHierarchyCreator.getJcrPath(BasePath.TAXONOMIES_TREE_STORAGE_PATH);      
+        Session session = 
+          uiUploadForm.getAncestorOfType(UIJCRExplorer.class).getSessionByWorkspace(workspaceName);
+        Node rootTree = (Node) session.getItem(rootTreePath);      
+        NodeIterator childrenIterator = rootTree.getNodes();
+        while (childrenIterator.hasNext()) {
+          Node childNode = childrenIterator.nextNode();
+          rootTreePath = childNode.getPath();
+          break;
+        }      
+        uiOneTaxonomySelector.setRootNodeLocation(repository, workspaceName, rootTreePath);
+        uiOneTaxonomySelector.setExceptedNodeTypesInPathPanel(new String[] {Utils.EXO_SYMLINK});
+        uiOneTaxonomySelector.init(uiExplorer.getSystemProvider());
+        String param = "returnField=" + FIELD_TAXONOMY ;
+        uiOneTaxonomySelector.setSourceComponent(uiUploadForm, new String[]{param});
+        uiPopupWindow.setRendered(true);
+        uiPopupWindow.setShow(true);
+        event.getRequestContext().addUIComponentToUpdateByAjax(uiUploadManager);
+      } catch (AccessDeniedException accessDeniedException) {
+        uiApp.addMessage(new ApplicationMessage("Taxonomy.msg.AccessDeniedException", null, 
+            ApplicationMessage.WARNING));
+        event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
+        return;
+      } catch (Exception e) {
+        uiApp.addMessage(new ApplicationMessage("Taxonomy.msg.AccessDeniedException", null, 
             ApplicationMessage.WARNING));
         event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages());
         return;
       }
-      UIUploadManager uiUploadManager = uiUploadForm.getParent();
-      UIJCRExplorer uiExplorer = uiUploadForm.getAncestorOfType(UIJCRExplorer.class);
-      NodeHierarchyCreator nodeHierarchyCreator = 
-        uiUploadForm.getApplicationComponent(NodeHierarchyCreator.class);
-      String repository = uiExplorer.getRepositoryName();
-      DMSConfiguration dmsConfig = uiUploadForm.getApplicationComponent(DMSConfiguration.class);
-      DMSRepositoryConfiguration dmsRepoConfig = dmsConfig.getConfig(repository);
-      String workspaceName = dmsRepoConfig.getSystemWorkspace();
-      
-      UIPopupWindow uiPopupWindow = uiUploadManager.initPopupTaxonomy(POPUP_TAXONOMY);
-      UIOneTaxonomySelector uiOneTaxonomySelector = 
-        uiUploadManager.createUIComponent(UIOneTaxonomySelector.class, null, null);
-      uiPopupWindow.setUIComponent(uiOneTaxonomySelector);
-      uiOneTaxonomySelector.setIsDisable(workspaceName, false);
-      String rootTreePath = nodeHierarchyCreator.getJcrPath(BasePath.TAXONOMIES_TREE_STORAGE_PATH);      
-      Session session = 
-        uiUploadForm.getAncestorOfType(UIJCRExplorer.class).getSessionByWorkspace(workspaceName);
-      Node rootTree = (Node) session.getItem(rootTreePath);      
-      NodeIterator childrenIterator = rootTree.getNodes();
-      while (childrenIterator.hasNext()) {
-        Node childNode = childrenIterator.nextNode();
-        rootTreePath = childNode.getPath();
-        break;
-      }      
-      uiOneTaxonomySelector.setRootNodeLocation(repository, workspaceName, rootTreePath);
-      uiOneTaxonomySelector.setExceptedNodeTypesInPathPanel(new String[] {Utils.EXO_SYMLINK});
-      uiOneTaxonomySelector.init(uiExplorer.getSystemProvider());
-      String param = "returnField=" + FIELD_TAXONOMY ;
-      uiOneTaxonomySelector.setSourceComponent(uiUploadForm, new String[]{param});
-      uiPopupWindow.setRendered(true);
-      uiPopupWindow.setShow(true);
-      event.getRequestContext().addUIComponentToUpdateByAjax(uiUploadManager);
-    }
+    }    
   }
 }
