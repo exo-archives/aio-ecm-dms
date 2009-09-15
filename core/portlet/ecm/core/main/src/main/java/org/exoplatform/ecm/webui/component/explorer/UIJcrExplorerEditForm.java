14f4e7d554a302e7ecc552b6d7ae3caa85ea0e56
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2009-09-15 08:14:48 +0000

    ECM-4249 Error when configuring for FE using Parameterize type without select drive

--- core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/UIJcrExplorerEditForm.java
+++ core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/UIJcrExplorerEditForm.java
@@ -312,6 +312,13 @@ public class UIJcrExplorerEditForm extends UIForm implements UISelectable {
       UIFormStringInput stringInputDrive = driveNameInput.getUIStringInput(UIJCRExplorerPortlet.DRIVE_NAME);
       String nodePath = ((UIFormStringInput)uiForm.findComponentById(UIJCRExplorerPortlet.PARAMETERIZE_PATH)).getValue();
       String driveName = stringInputDrive.getValue();
+      if ((driveName == null) || (driveName.length() == 0)) {
+        UIApplication uiApp = uiForm.getAncestorOfType(UIApplication.class);
+        uiApp.addMessage(new ApplicationMessage("UIJcrExplorerEditForm.msg.notNullDriveName", null, 
+            ApplicationMessage.WARNING));
+        event.getRequestContext().addUIComponentToUpdateByAjax(uiForm);
+        return;
+      }
       String useCase = typeSelectBox.getValue();
       if (useCase.equals(UIJCRExplorerPortlet.SELECTION)) {
         driveName = "";
@@ -352,9 +359,10 @@ public class UIJcrExplorerEditForm extends UIForm implements UISelectable {
       String driveName = stringInputDrive.getValue();
       if (driveName == null || driveName.length() == 0) {
         UIApplication uiApp = uiForm.getAncestorOfType(UIApplication.class);
-        uiApp.addMessage(new ApplicationMessage("UIJcrExplorerEditForm.msg.selectdrive", null));
+        uiApp.addMessage(new ApplicationMessage("UIJcrExplorerEditForm.msg.notNullDriveName", null, 
+            ApplicationMessage.WARNING));
+        return;
       }
-      
       editContainer.initPopupDriveBrowser(POPUP_SELECT_PATH_INPUT, driveName);
       event.getRequestContext().addUIComponentToUpdateByAjax(uiForm.getParent());
     }
