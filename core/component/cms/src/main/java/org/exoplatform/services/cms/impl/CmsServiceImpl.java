988bfa995f8d2c1ad6fac1bd87900eb4d1c7ccec
Hoang Van Hung <hung.van@exoplatform.com>
2009-09-25 10:12:58 +0000

    ECM-4284 Can't fire event when create document in Validation Request folder: commit for 2.5.x

--- core/component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
+++ core/component/cms/src/main/java/org/exoplatform/services/cms/impl/CmsServiceImpl.java
@@ -598,6 +598,7 @@ public class CmsServiceImpl implements CmsService {
               node.setProperty(propertyName, session.getValueFactory().createValue(value.toString()));
             }
         }
+        session.save();
         session.logout();
       } else if(value instanceof String[]) {
         String[] values = (String[]) value;        
@@ -834,6 +835,7 @@ public class CmsServiceImpl implements CmsService {
             }
           }
         }
+        session.save();
         session.logout();
       } else if(value instanceof String[]) {
         String[] values = (String[]) value;        
@@ -868,6 +870,7 @@ public class CmsServiceImpl implements CmsService {
         if (!property.getValues().equals(list.toArray(new Value[list.size()]))) {
           node.setProperty(propertyName, list.toArray(new Value[list.size()]));
         }
+        session.save();
         session.logout();
       }       
       break ;
