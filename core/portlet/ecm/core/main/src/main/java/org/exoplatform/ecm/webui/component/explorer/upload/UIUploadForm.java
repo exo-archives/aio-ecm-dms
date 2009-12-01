99f2c74010b771ec3abca4dfb080d5339f945a0c
Nguyen Van Chien <chien.nguyen@exoplatform.com>
2009-12-01 10:16:11 +0000

    ECM-4445 All files are disappeared when delete blank form in Upload form

--- core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/upload/UIUploadForm.java
+++ core/portlet/ecm/core/main/src/main/java/org/exoplatform/ecm/webui/component/explorer/upload/UIUploadForm.java
@@ -337,6 +337,31 @@ public class UIUploadForm extends UIForm implements UIPopupComponent, UISelectab
     String pers = PermissionType.ADD_NODE + "," + PermissionType.SET_PROPERTY ;
     selectedNode.getSession().checkPermission(selectedNode.getPath(), pers);
     try {
+      for (UIComponent uiCompValidate : listFormChildren) {
+        if(uiCompValidate instanceof UIFormUploadInput) {
+          String[] arrayId = uiCompValidate.getId().split(FIELD_UPLOAD);
+          if ((arrayId.length > 0) && (arrayId[0].length() > 0)) index = new Integer(arrayId[0]).intValue();
+          UIFormUploadInput uiFormUploadInput;  
+          if (index == 0){
+            uiFormUploadInput = (UIFormUploadInput) getUIInput(FIELD_UPLOAD);
+          } else {
+            uiFormUploadInput = (UIFormUploadInput) getUIInput(index + FIELD_UPLOAD);
+          }
+          if(uiFormUploadInput.getUploadResource() == null) {            
+            uiApp.addMessage(new ApplicationMessage("UIUploadForm.msg.fileName-error", null, 
+                                                    ApplicationMessage.WARNING)) ;
+            event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages()) ;
+            return ;
+          }
+          String fileName = uiFormUploadInput.getUploadResource().getFileName();
+          if(fileName == null || fileName.length() == 0) {
+            uiApp.addMessage(new ApplicationMessage("UIUploadForm.msg.fileName-error", null, 
+                                                    ApplicationMessage.WARNING)) ;
+            event.getRequestContext().addUIComponentToUpdateByAjax(uiApp.getUIPopupMessages()) ;
+            return;
+          }
+        }
+      }
       for (UIComponent uiComp : listFormChildren) {
         if(uiComp instanceof UIFormUploadInput) {
           String[] arrayId = uiComp.getId().split(FIELD_UPLOAD);
