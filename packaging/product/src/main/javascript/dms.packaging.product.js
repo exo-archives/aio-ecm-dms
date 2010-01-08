b316ba52f50f90d5ef487d850f79021a774cfbf6
Minh Van Dang <minh.dang@exoplatform.com>
2010-01-08 04:09:56 +0000

    ECM-4295 Create packaing and update pom to upgrade to GateIn

---
+++ packaging/product/src/main/javascript/dms.packaging.product.js
@@ -0,0 +1,47 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getProduct(version) {
+  var product = new Product();
+  
+  product.name = "eXoDMS" ;
+  product.portalwar = "portal.war" ;
+  product.codeRepo = "ecm/dms" ;
+  product.useContentvalidation = true;
+  product.version = "${project.version}" ;
+  product.contentvalidationVersion = "${project.version}";
+  product.workflowVersion = "${org.exoplatform.ecm.workflow.version}" ;
+  product.serverPluginVersion = "${org.exoplatform.portal.version}" ;
+  product.workflowJbpmVersion = "${org.exoplatform.ecm.jbpm.version}";
+  product.workflowBonitaVersion = "${org.bonita.version}";
+
+  var kernel = Module.GetModule("kernel") ;
+  var core = Module.GetModule("core") ;
+  var ws = Module.GetModule("ws");
+  var eXoJcr = Module.GetModule("jcr", {kernel : kernel, core : core, ws : ws}) ;
+  var portal = Module.GetModule("portal", {kernel : kernel, ws:ws, core : core, eXoJcr : eXoJcr });
+  var dms = Module.GetModule("dms", {kernel : kernel, core : core, ws : ws, eXoJcr : eXoJcr, portal : portal});
+    
+  product.addDependencies(portal.web.rest);
+  product.addDependencies(dms.web.dmsportal);
+  product.addDependencies(dms.web.eXoDMSResources);
+  product.addDependencies(portal.portlet.exoadmin);
+  product.addDependencies(portal.portlet.web);
+  product.addDependencies(portal.portlet.dashboard);
+	product.addDependencies(portal.eXoGadgetServer);
+	product.addDependencies(portal.eXoGadgets);  
+  product.addDependencies(dms.portlet.dms);
+  product.addDependencies(dms.portlet.jcr_console);
+  product.addDependencies(dms.gadgets);
+    
+  product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
+  product.addServerPatch("jboss",  portal.server.jboss.patch) ;
+  product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;  
+  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
+  product.addServerPatch("ear",  portal.server.websphere.patch) ;
+
+  product.module = dms ;
+  product.dependencyModule = [kernel, core, ws, eXoJcr, portal];
+
+  return product ;
+}
