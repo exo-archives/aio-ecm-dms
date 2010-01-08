f80948e6cc2012f24498f3dc9b2e1cc02970c0aa
Phan Le Thanh Chuong <chuong.phan@exoplatform.com>
2010-01-08 10:10:41 +0000

    ECM-4295: Make DMS 3.0 relies on GateIn

--- core/web/eXoDMSResources/src/main/webapp/WEB-INF/conf/script/groovy/SkinConfigScript.groovy
+++
@@ -1,3 +0,0 @@
-SkinService.addPortalSkin("DMSSkin","Default", "/eXoDMSResources/skin/DefaultSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("DMSSkin","Mac", "/eXoDMSResources/skin/MacSkin/Stylesheet.css", ServletContext);
-SkinService.addPortalSkin("DMSSkin","Vista", "/eXoDMSResources/skin/VistaSkin/Stylesheet.css", ServletContext);
