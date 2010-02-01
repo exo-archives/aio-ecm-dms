590d6fc0acd091f0f06afd8a6841dfd917b260c3
Hoang Van Hung <hung.van@exoplatform.com>
2010-02-01 09:20:07 +0000

    ECM-5179 can't view nt:file

--- packaging/module/src/main/javascript/dms.packaging.module.js
+++ packaging/module/src/main/javascript/dms.packaging.module.js
@@ -21,6 +21,7 @@ function getModule(params) {
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.cms", "jar",  module.version)) .     
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.deployment", "jar",  module.version)) .    
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.publication", "jar", module.version)).
+    addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.component.document.viewer", "jar", module.version)).
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.connector.fckeditor", "jar", module.version)).
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.dms", "jar", module.version)).
     addDependency(new Project("org.exoplatform.ecm.dms.core", "exo.ecm.dms.core.webui.ext", "jar", module.version)).
